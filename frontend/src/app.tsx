2aca7b04e1248c5393fbe0fc3de3165a555935ad
Philippe Voinov <philippevoinov@gmail.com>
2018-04-20 13:05:05 +0200

    React to window resizes.

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,6 +3,7 @@ import { renderDocument, SectionRenderer } from "./split-render";
 import { Section, SectionKind } from "./interfaces";
 import Exam from "./exam";
 import * as pdfjs from "pdfjs-dist";
+import { debounce } from "lodash";
 
 const SECTIONS: Section[] = [
   {
@@ -23,22 +24,27 @@ const SECTIONS: Section[] = [
   },
 ];
 
-const WIDTH = 800;
+const RERENDER_INTERVAL = 500;
 
 interface State {
   pdf?: pdfjs.PDFDocumentProxy;
   renderer?: SectionRenderer;
+  width: number;
   dpr: number;
 }
 
 export default class App extends React.Component<{}, State> {
   state: State = {
+    width: window.innerWidth,
     dpr: window.devicePixelRatio,
   };
   updateInverval: NodeJS.Timer;
+  debouncedRender: this["renderDocument"];
 
   async componentWillMount() {
-    this.updateInverval = setInterval(this.updateZoom, 500);
+    this.updateInverval = setInterval(this.pollZoom, RERENDER_INTERVAL);
+    window.addEventListener("resize", this.onResize);
+    this.debouncedRender = debounce(this.renderDocument, RERENDER_INTERVAL);
 
     // tslint:disable-next-line:no-any
     const PDFJS: pdfjs.PDFJSStatic = pdfjs as any;
@@ -48,9 +54,22 @@ export default class App extends React.Component<{}, State> {
 
   componentWillUnmount() {
     clearInterval(this.updateInverval);
+    window.removeEventListener("resize", this.onResize);
   }
 
-  updateZoom = () => {
+  onResize = () => {
+    const w = window.innerWidth;
+    if (w === this.state.width) {
+      return;
+    }
+    this.setState({ width: w });
+    const { pdf } = this.state;
+    if (pdf) {
+      this.debouncedRender(pdf);
+    }
+  };
+
+  pollZoom = () => {
     const dpr = window.devicePixelRatio;
     if (dpr === this.state.dpr) {
       return;
@@ -63,17 +82,17 @@ export default class App extends React.Component<{}, State> {
   };
 
   async renderDocument(pdf: pdfjs.PDFDocumentProxy) {
-    const w = WIDTH * this.state.dpr;
+    const w = this.state.width * this.state.dpr;
     this.setState({ pdf, renderer: await renderDocument(pdf, w) });
   }
 
   render() {
-    const { renderer, dpr } = this.state;
+    const { renderer, width, dpr } = this.state;
     if (!renderer) {
       return <div>Loading...</div>;
     }
     return (
-      <Exam sections={SECTIONS} renderer={renderer} width={WIDTH} dpr={dpr} />
+      <Exam sections={SECTIONS} renderer={renderer} width={width} dpr={dpr} />
     );
   }
 }
