beb262d42d1b6427b6f53d3af09e5f9f6ec7c73e
Benjamin Schmid <git@schmidb.ch>
2020-02-28 14:47:47 +0100

    add category tests

--- backend/categories/tests.py
+++ backend/categories/tests.py
@@ -1,4 +1,6 @@
-from testing.tests import ComsolTest
+from answers.models import Exam
+from myauth.models import MyUser
+from testing.tests import ComsolTest, ComsolTestExamsData
 from categories.models import Category
 
 
@@ -63,8 +65,92 @@ class TestList(ComsolTest):
         self.assertEqual(res[0]['slug'], self.cat1.slug)
 
 
-# TODO: test list_exams, checking whether all counts are correct
-# TODO: test withmeta, checking whether all counts are correct
-# TODO: test setting and getting metadata
-# TODO: test adding and removing users to sets (admin, experts)
+class TestMetadata(ComsolTest):
+
+    def mySetUp(self):
+        self.cat1 = Category(
+            displayname='Test 1',
+            slug='test1',
+            remark='Test remark',
+            semester='HS',
+            has_payments=True
+        )
+        self.cat1.save()
+
+    def test_get_metadata(self):
+        res = self.get('/api/category/metadata/test1/')['value']
+        self.assertEqual(res['displayname'], self.cat1.displayname)
+        self.assertEqual(res['slug'], self.cat1.slug)
+        self.assertEqual(res['remark'], self.cat1.remark)
+        self.assertEqual(res['semester'], self.cat1.semester)
+        self.assertEqual(res['has_payments'], self.cat1.has_payments)
+
+    def test_set_metadata(self):
+        self.post('/api/category/setmetadata/test1/', {
+            'remark': 'New test remark',
+            'semester': 'FS',
+            'has_payments': False,
+        })
+        self.cat1.refresh_from_db()
+        self.assertEqual(self.cat1.remark, 'New test remark')
+        self.assertEqual(self.cat1.semester, 'FS')
+        self.assertEqual(self.cat1.has_payments, False)
+
+    def test_set_slug(self):
+        self.post('/api/category/setmetadata/test1/', {
+            'slug': 'newslug'
+        })
+        self.cat1.refresh_from_db()
+        self.assertEqual(self.cat1.slug, 'test1')
+
+    def test_add_remove_user(self):
+        user, _ = MyUser.objects.get_or_create(username='morica')
+        self.post('/api/category/addusertoset/test1/', {
+            'key': 'admins',
+            'user': 'morica',
+        })
+        self.cat1.refresh_from_db()
+        self.assertTrue(user in self.cat1.admins.all())
+        self.assertFalse(user in self.cat1.experts.all())
+        self.post('/api/category/removeuserfromset/test1/', {
+            'key': 'admins',
+            'user': 'morica',
+        })
+        self.cat1.refresh_from_db()
+        self.assertFalse(user in self.cat1.admins.all())
+        self.assertFalse(user in self.cat1.experts.all())
+        self.post('/api/category/addusertoset/test1/', {
+            'key': 'experts',
+            'user': 'morica',
+        })
+        self.cat1.refresh_from_db()
+        self.assertFalse(user in self.cat1.admins.all())
+        self.assertTrue(user in self.cat1.experts.all())
+        self.post('/api/category/removeuserfromset/test1/', {
+            'key': 'experts',
+            'user': 'morica',
+        })
+        self.cat1.refresh_from_db()
+        self.assertFalse(user in self.cat1.admins.all())
+        self.assertFalse(user in self.cat1.experts.all())
+
+
+class TestListExams(ComsolTestExamsData):
+
+    def test_list_exams(self):
+        self.exams[1].public = False
+        self.exams[1].save()
+        res = self.get('/api/category/listexams/TestCategory/')['value']
+        self.assertEqual(len(res), 3)
+        self.assertTrue(res[0]['public'])
+        self.assertFalse(res[1]['public'])
+        self.assertTrue(res[2]['public'])
+
+
+class TestMetaCategories(ComsolTest):
+    pass
+
+
+# TODO: test whether the counts returned in list_exams and withmeta are correct
+
 # TODO: test adding, removing, and retrieving meta categories
