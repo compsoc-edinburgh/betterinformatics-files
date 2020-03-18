b1b07faab88ace3dc3c701ade671c8acff24f42e
lmoeller <lmoeller@student.ethz.ch>
2020-03-18 12:47:39 +0100

    Add debug login select

--- frontend/src/components/login-card.tsx
+++ frontend/src/components/login-card.tsx
@@ -5,8 +5,12 @@ import {
   Card,
   CardBody,
   CardHeader,
+  Col,
   Form,
+  FormGroup,
   InputField,
+  Row,
+  Select,
 } from "@vseth/components";
 import React, { useState } from "react";
 import { useSetUser } from "../auth";
@@ -24,6 +28,16 @@ const login = async (
   });
 };
 
+const debugLogins = {
+  schneij: "UOmtnC7{'%G",
+  fletchz: "123456abc",
+  morica: "admin666",
+};
+const options = Object.keys(debugLogins).map(username => ({
+  value: username,
+  label: username,
+}));
+
 const LoginCard: React.FC<{}> = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -32,8 +46,8 @@ const LoginCard: React.FC<{}> = () => {
   const { loading, run: tryLogin } = useRequest(login, {
     manual: true,
     onSuccess: e => {
-      setUser(undefined);
       setError(undefined);
+      setUser(undefined);
     },
     onError: e => setError(e.toString()),
   });
@@ -68,9 +82,31 @@ const LoginCard: React.FC<{}> = () => {
             disabled={loading}
             required
           />
-          <Button color="primary" type="submit" disabled={loading}>
-            Submit
-          </Button>
+          <Row form>
+            <Col md={4}>
+              {" "}
+              <FormGroup>
+                <Button color="primary" type="submit" disabled={loading}>
+                  Submit
+                </Button>
+              </FormGroup>
+            </Col>
+            <Col md={8}>
+              <FormGroup>
+                {window.location.hostname === "localhost" && (
+                  <Select
+                    options={options}
+                    onChange={({ value }: any) => {
+                      setUsername(value);
+                      setPassword(
+                        debugLogins[value as keyof typeof debugLogins],
+                      );
+                    }}
+                  />
+                )}
+              </FormGroup>
+            </Col>
+          </Row>
         </Form>
       </CardBody>
     </Card>
