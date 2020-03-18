7c4c550791e162f5083dad29267d718622499cf3
lmoeller <lmoeller@student.ethz.ch>
2020-03-18 10:35:00 +0100

    Add admin menu items

--- frontend/src/components/exams-navbar.tsx
+++ frontend/src/components/exams-navbar.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { VSETHNavbar as Navbar, NavbarBrand, ICONS } from "@vseth/components";
 import { useLocation } from "react-router-dom";
+import { useUser } from "../auth";
+import { Item } from "@vseth/components/dist/components/VSETHNav/VSETHNavbar";
+
 const ExamsNavbar: React.FC<{}> = () => {
   const location = useLocation();
+  const user = useUser();
+  const adminItems: Item[] = [
+    {
+      title: "Upload Exam",
+    },
+    {
+      title: "Mod Queue",
+    },
+  ];
   return (
     <Navbar
       lang={"en"}
@@ -32,6 +44,17 @@ const ExamsNavbar: React.FC<{}> = () => {
           linkProps: {
             to: "/scoreboard",
           },
+        },
+        {
+          title: "More",
+          icon: ICONS.DOTS_H,
+          active: false,
+          childItems: [
+            {
+              title: "Submit Transcript",
+            },
+            ...(typeof user === "object" && user.isAdmin ? adminItems : []),
+          ],
         },
         {
           title: "Account",
