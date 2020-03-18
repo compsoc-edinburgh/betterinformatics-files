5b2f9b38f172ded062989cabfe5c5246857830f9
lmoeller <lmoeller@student.ethz.ch>
2020-03-18 10:35:21 +0100

    Start implementing feedback admin UI

--- frontend/src/pages/feedback-page.tsx
+++ frontend/src/pages/feedback-page.tsx
@@ -1,28 +1,55 @@
 import React, { useState } from "react";
-import { Container, Input, Button, FormGroup } from "@vseth/components";
+import {
+  Container,
+  Button,
+  FormGroup,
+  Spinner,
+  Input,
+  Nav,
+  NavLink,
+  NavItem,
+} from "@vseth/components";
 import { Link } from "react-router-dom";
+import { fetchpost } from "../fetch-utils";
+import { useRequest, useLocalStorageState } from "@umijs/hooks";
+import { useUser, User } from "../auth";
 
-const FeedbackPage: React.FC<{}> = () => {
+enum AdminMode {
+  Read,
+  Write,
+}
+
+const submitFeedback = async (text: string) => {
+  return await fetchpost("api/feedback/submit", { text });
+};
+
+const FeedbackForm: React.FC<{}> = () => {
   const [text, setText] = useState("");
+  const { loading, run } = useRequest(submitFeedback, {
+    manual: true,
+    onSuccess() {
+      setText("");
+    },
+  });
+
   return (
-    <Container>
-      <h2>Feedback</h2>
+    <>
       <p>
         Please tell us what you think about the new Community Solutions! What do
         you like? What could we improve? Ideas for new features?
       </p>
       <p>
         Use the form below or write to{" "}
-        <Link to="mailto:communitysolutions@vis.ethz.ch">
+        <a href="mailto:communitysolutions@vis.ethz.ch">
           communitysolutions@vis.ethz.ch
-        </Link>
+        </a>
         .
       </p>
       <p>
         To report issues with the platform you can open an issue in our{" "}
-        <Link to="https://gitlab.ethz.ch/vis/cat/community-solutions/issues">
+        <a href="https://gitlab.ethz.ch/vis/cat/community-solutions/issues">
           issue tracker
-        </Link>
+        </a>
         .
       </p>
       <FormGroup>
@@ -34,10 +61,54 @@ const FeedbackPage: React.FC<{}> = () => {
         />
       </FormGroup>
       <FormGroup>
-        <Button disabled={text.length === 0} loading={true}>
-          Send
+        <Button
+          disabled={text.length === 0 || loading}
+          onClick={() => run(text)}
+        >
+          {loading ? <Spinner /> : "Submit"}
         </Button>
       </FormGroup>
+    </>
+  );
+};
+const FeedbackAdminView: React.FC<{}> = () => {
+  const [mode, setMode] = useLocalStorageState<AdminMode>(
+    "feedback-admin-mode",
+    AdminMode.Read,
+  );
+  return (
+    <Container>
+      <h2>Feedback</h2>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={mode === AdminMode.Read ? "active" : ""}
+            onClick={() => setMode(AdminMode.Read)}
+          >
+            Read
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={mode === AdminMode.Write ? "active" : ""}
+            onClick={() => setMode(AdminMode.Write)}
+          >
+            Write
+          </NavLink>
+        </NavItem>
+      </Nav>
+      {mode === AdminMode.Read ? null : <FeedbackForm />}
+    </Container>
+  );
+};
+const FeedbackPage: React.FC<{}> = () => {
+  const { isAdmin } = useUser() as User;
+  return isAdmin ? (
+    <FeedbackAdminView />
+  ) : (
+    <Container>
+      <h2>Feedback</h2>
+      <FeedbackForm />
     </Container>
   );
 };
