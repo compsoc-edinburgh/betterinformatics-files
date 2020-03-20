d91ddc34611adfe6ff6e566209c5a4cd11ea09c0
lmoeller <lmoeller@student.ethz.ch>
2020-03-20 19:12:49 +0100

    Added Notification

--- frontend/src/components/notification.tsx
+++ frontend/src/components/notification.tsx
@@ -1,79 +1,57 @@
-import * as React from "react";
-import { NotificationInfo } from "../interfaces";
+import { useInViewport } from "@umijs/hooks";
+import { Card, CardBody, CardHeader, Alert } from "@vseth/components";
 import moment from "moment";
-import { css } from "glamor";
-import { fetchpost } from "../fetch-utils";
-import Colors from "../colors";
+import * as React from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import MarkdownText from "./markdown-text";
-import globalcss from "../globalcss";
 import GlobalConsts from "../globalconsts";
-
+import { useMarkAsRead } from "../hooks/api";
+import { NotificationInfo } from "../interfaces";
+import MarkdownText from "./markdown-text";
 interface Props {
   notification: NotificationInfo;
 }
 
-const styles = {
-  wrapper: css({
-    background: Colors.cardBackground,
-    padding: "10px",
-    marginBottom: "20px",
-    boxShadow: Colors.cardShadow,
-    maxWidth: "500px",
-    "@media (max-width: 699px)": {
-      padding: "5px",
-    },
-  }),
-  header: css({
-    fontSize: "24px",
-    marginBottom: "10px",
-    marginLeft: "-10px",
-    marginRight: "-10px",
-    marginTop: "-10px",
-    padding: "10px",
-    background: Colors.cardHeader,
-    color: Colors.cardHeaderForeground,
-  }),
-  subtitle: css({
-    fontSize: "16px",
-  }),
-  unread: css({
-    fontWeight: "bold",
-  }),
+const NotificationComponent: React.FC<Props> = ({ notification }) => {
+  const [inViewport, ref] = useInViewport<HTMLDivElement>();
+  const visible = inViewport || false;
+  const [read, setRead] = useState(notification.read);
+  useEffect(() => {
+    setRead(notification.read);
+  }, [notification]);
+  const [error, , markAsRead] = useMarkAsRead();
+  useEffect(() => {
+    if (visible && !read) {
+      setRead(true);
+      markAsRead(notification.oid);
+    }
+  }, [markAsRead, read, visible, notification.oid]);
+  return (
+    <div ref={ref}>
+      {error && <Alert color="danger">{error.message}</Alert>}
+      <Card style={{ marginTop: "2em", marginBottom: "2em" }}>
+        <CardHeader>
+          <h6>
+            <Link to={notification.link}>{notification.title}</Link>
+            <div>
+              <small>
+                <Link to={notification.sender}>
+                  {notification.senderDisplayName}
+                </Link>{" "}
+                •{" "}
+                {moment(
+                  notification.time,
+                  GlobalConsts.momentParseString,
+                ).format(GlobalConsts.momentFormatString)}
+              </small>{" "}
+            </div>
+          </h6>
+        </CardHeader>
+        <CardBody>
+          <MarkdownText value={notification.message} />
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
-
-export default class NotificationComponent extends React.Component<Props> {
-  readNotification = (notification: NotificationInfo) => {
-    fetchpost("/api/notification/setread/" + notification.oid + "/", {
-      read: true,
-    });
-  };
-
-  render() {
-    const { notification } = this.props;
-    return (
-      <div {...styles.wrapper}>
-        <Link
-          to={notification.link}
-          onClick={() => this.readNotification(notification)}
-        >
-          <div {...styles.header}>
-            <div {...(notification.read ? undefined : styles.unread)}>
-              {notification.title}
-            </div>
-            <div {...globalcss.noLinkColor} {...styles.subtitle}>
-              <Link to={notification.sender}>
-                {notification.senderDisplayName}
-              </Link>{" "}
-              •{" "}
-              {moment(notification.time, GlobalConsts.momentParseString).format(
-                GlobalConsts.momentFormatString,
-              )}
-            </div>
-          </div>
-        </Link>
-        <MarkdownText value={notification.message} />
-      </div>
-    );
-  }
-}
+export default NotificationComponent;
