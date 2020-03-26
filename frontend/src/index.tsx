27107da89f47bb006c14ac88f2be629b211df89e
lmoeller <lmoeller@student.ethz.ch>
2020-03-26 16:50:56 +0100

    Add server data

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./app";
-//import registerServiceWorker from "./register-service-worker";
 import { BrowserRouter } from "react-router-dom";
 import "@vseth/vseth-theme/dist/vseth-bootstrap-theme.css";
 
@@ -11,4 +10,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root") as HTMLElement,
 );
-//registerServiceWorker();
