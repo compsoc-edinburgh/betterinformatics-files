eeb09084c102c18715b26ca28fca66cea7c944a1
lmoeller <lmoeller@student.ethz.ch>
2020-04-04 00:08:55 +0200

    Simplify metadata editor

--- frontend/src/components/exam-metadata-editor.tsx
+++ frontend/src/components/exam-metadata-editor.tsx
@@ -1,42 +1,45 @@
-import { ExamMetaData } from "../interfaces";
-import useInitialState from "../hooks/useInitialState";
-import { createOptions, options, SelectOption } from "../ts-utils";
 import { useRequest } from "@umijs/hooks";
 import {
+  Alert,
+  Button,
   Card,
   CardBody,
-  Button,
-  Alert,
-  Row,
+  CardFooter,
   Col,
   FormGroup,
   Input,
+  Label,
+  Row,
   Select,
   TextareaField,
-  Label,
-  CardFooter,
 } from "@vseth/components";
 import React from "react";
-import AttachmentsEditor, { EditorAttachment } from "./attachments-editor";
-import { loadCategories } from "../api/hooks";
-import FileInput from "./file-input";
-import TwoButtons from "./two-buttons";
-import IconButton from "./icon-button";
 import { fetchpost } from "../api/fetch-utils";
-/*
-    'displayname',
-    'category',
-    'examtype',
-    'legacy_solution',
-    'master_solution',
-    'resolve_alias',
-    'remark',
-    'public',
-    'finished_cuts',
-    'finished_wiki_transfer',
-    'needs_payment',
-    'solution_printonly',
-*/
+import { loadCategories } from "../api/hooks";
+import useInitialState from "../hooks/useInitialState";
+import { ExamMetaData } from "../interfaces";
+import { createOptions, options, SelectOption } from "../ts-utils";
+import AttachmentsEditor, { EditorAttachment } from "./attachments-editor";
+import FileInput from "./file-input";
+import IconButton from "./icon-button";
+import TwoButtons from "./two-buttons";
+const stringKeys = [
+  "displayname",
+  "category",
+  "examtype",
+  "legacy_solution",
+  "master_solution",
+  "resolve_alias",
+  "remark",
+] as const;
+const booleanKeys = [
+  "public",
+  "finished_cuts",
+  "finished_wiki_transfer",
+  "needs_payment",
+  "solution_printonly",
+] as const;
+
 const setMetaData = async (
   filename: string,
   changes: Partial<ExamMetaData>,
@@ -57,26 +60,12 @@ const applyChanges = async (
   newMetaData: ExamMetaDataDraft,
 ) => {
   const metaDataDiff: Partial<ExamMetaData> = {};
-  if (oldMetaData.displayname !== newMetaData.displayname)
-    metaDataDiff.displayname = newMetaData.displayname;
-  if (oldMetaData.resolve_alias !== newMetaData.resolve_alias)
-    metaDataDiff.resolve_alias = newMetaData.resolve_alias;
-  if (oldMetaData.category !== newMetaData.category)
-    metaDataDiff.category = newMetaData.category;
-  if (oldMetaData.examtype !== newMetaData.examtype)
-    metaDataDiff.examtype = newMetaData.examtype;
-  if (oldMetaData.legacy_solution !== newMetaData.legacy_solution)
-    metaDataDiff.legacy_solution = newMetaData.legacy_solution;
-  if (oldMetaData.master_solution !== newMetaData.master_solution)
-    metaDataDiff.master_solution = newMetaData.master_solution;
-  if (oldMetaData.remark !== newMetaData.remark)
-    metaDataDiff.remark = newMetaData.remark;
-  if (oldMetaData.public !== newMetaData.public)
-    metaDataDiff.public = newMetaData.public;
-  if (oldMetaData.needs_payment !== newMetaData.needs_payment)
-    metaDataDiff.needs_payment = newMetaData.needs_payment;
-  if (oldMetaData.finished_cuts !== newMetaData.finished_cuts)
-    metaDataDiff.finished_cuts = newMetaData.finished_cuts;
+  for (const key of stringKeys)
+    if (oldMetaData[key] !== newMetaData[key])
+      metaDataDiff[key] = newMetaData[key];
+  for (const key of booleanKeys)
+    if (oldMetaData[key] !== newMetaData[key])
+      metaDataDiff[key] = newMetaData[key];
   await setMetaData(filename, metaDataDiff);
   return {
     ...oldMetaData,
@@ -113,57 +102,26 @@ const ExamMetadataEditor: React.FC<Props> = ({
       onMetaDataChange(newMetaData);
     },
   });
-  const [displayName, setDisplayName] = useInitialState(
-    currentMetaData.displayname,
+  const [draftState, setDraftState] = useInitialState<ExamMetaDataDraft>(
+    currentMetaData,
   );
-  const [resolveAlias, setResolveAlias] = useInitialState(
-    currentMetaData.resolve_alias,
-  );
-  const [category, setCategory] = useInitialState(currentMetaData.category);
-  const [examType, setExamType] = useInitialState<keyof typeof examTypeOptions>(
-    currentMetaData.examtype as keyof typeof examTypeOptions,
-  );
-  const [legacySolution, setLegacySolution] = useInitialState(
-    currentMetaData.legacy_solution,
-  );
-  const [masterSolution, setMasterSolution] = useInitialState(
-    currentMetaData.master_solution,
-  );
-  const [remark, setRemark] = useInitialState(currentMetaData.remark);
-  const [isPublic, setPublic] = useInitialState(currentMetaData.public);
-  const [needsPayment, setNeedsPayment] = useInitialState(
-    currentMetaData.needs_payment,
-  );
-  const [finishedCuts, setFinishedCuts] = useInitialState(
-    currentMetaData.finished_cuts,
-  );
-  const [finishedWiki, setFinishedWiki] = useInitialState(
-    currentMetaData.finished_wiki_transfer,
-  );
+  const setKey = <T, K extends keyof ExamMetaDataDraft>(
+    key: K,
+    value: ExamMetaDataDraft[K],
+  ) => {
+    setDraftState(prevState => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
   const [printonlyFile, setPrintonlyFile] = useInitialState<File | undefined>(
     undefined,
   );
   const [masterFile, setMasterFile] = useInitialState<File | undefined>(
     undefined,
   );
-  const [attachments, setAttachments] = useInitialState<EditorAttachment[]>(
-    currentMetaData.attachments,
-  );
   const save = () => {
-    runApplyChanges(currentMetaData.filename, currentMetaData, {
-      ...currentMetaData,
-      displayname: displayName,
-      resolve_alias: resolveAlias,
-      category: category,
-      examtype: examType,
-      legacy_solution: legacySolution,
-      master_solution: masterSolution,
-      remark,
-      public: isPublic,
-      needs_payment: needsPayment,
-      finished_cuts: finishedCuts,
-      finished_wiki_transfer: finishedWiki,
-    });
+    runApplyChanges(currentMetaData.filename, currentMetaData, draftState);
   };
 
   return (
@@ -178,8 +136,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
               <label className="form-input-label">Display name</label>
               <Input
                 type="text"
-                value={displayName}
-                onChange={e => setDisplayName(e.currentTarget.value)}
+                value={draftState.displayname}
+                onChange={e => setKey("displayname", e.currentTarget.value)}
               />
             </FormGroup>
           </Col>
@@ -188,8 +146,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
               <label className="form-input-label">Resolve alias</label>
               <Input
                 type="text"
-                value={resolveAlias}
-                onChange={e => setResolveAlias(e.currentTarget.value)}
+                value={draftState.resolve_alias}
+                onChange={e => setKey("resolve_alias", e.currentTarget.value)}
               />
             </FormGroup>
           </Col>
@@ -202,8 +160,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
                 options={
                   categoryOptions ? (options(categoryOptions) as any) : []
                 }
-                value={categoryOptions && categoryOptions[category]}
-                onChange={(e: any) => setCategory(e.value as string)}
+                value={categoryOptions && categoryOptions[draftState.category]}
+                onChange={(e: any) => setKey("category", e.value as string)}
                 isLoading={categoriesLoading}
                 required
               />
@@ -214,9 +172,14 @@ const ExamMetadataEditor: React.FC<Props> = ({
               <label className="form-input-label">Exam type</label>
               <Select
                 options={options(examTypeOptions)}
-                value={examTypeOptions[examType]}
+                value={
+                  examTypeOptions[
+                    draftState.examtype as keyof typeof examTypeOptions
+                  ]
+                }
                 onChange={option =>
-                  setExamType(
+                  setKey(
+                    "examtype",
                     (option as SelectOption<typeof examTypeOptions>).value,
                   )
                 }
@@ -231,8 +194,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
                 type="checkbox"
                 name="check"
                 id="isPublic"
-                checked={isPublic}
-                onChange={e => setPublic(e.currentTarget.checked)}
+                checked={draftState.public}
+                onChange={e => setKey("public", e.currentTarget.checked)}
               />
               <Label for="isPublic" check>
                 Public
@@ -245,8 +208,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
                 type="checkbox"
                 name="check"
                 id="needsPayment"
-                checked={needsPayment}
-                onChange={e => setNeedsPayment(e.currentTarget.checked)}
+                checked={draftState.needs_payment}
+                onChange={e => setKey("needs_payment", e.currentTarget.checked)}
               />
               <Label for="needsPayment" check>
                 Needs Payment
@@ -261,8 +224,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
                 type="checkbox"
                 name="check"
                 id="cuts"
-                checked={finishedCuts}
-                onChange={e => setFinishedCuts(e.currentTarget.checked)}
+                checked={draftState.finished_cuts}
+                onChange={e => setKey("finished_cuts", e.currentTarget.checked)}
               />
               <Label for="cuts" check>
                 Finished Cuts
@@ -275,8 +238,10 @@ const ExamMetadataEditor: React.FC<Props> = ({
                 type="checkbox"
                 name="check"
                 id="wiki"
-                checked={finishedWiki}
-                onChange={e => setFinishedWiki(e.currentTarget.checked)}
+                checked={draftState.finished_wiki_transfer}
+                onChange={e =>
+                  setKey("finished_wiki_transfer", e.currentTarget.checked)
+                }
               />
               <Label for="wiki" check>
                 Finished Wiki Transfer
@@ -290,8 +255,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
               <label className="form-input-label">Legacy Solution</label>
               <Input
                 type="url"
-                value={legacySolution}
-                onChange={e => setLegacySolution(e.currentTarget.value)}
+                value={draftState.legacy_solution}
+                onChange={e => setKey("legacy_solution", e.currentTarget.value)}
               />
             </FormGroup>
           </Col>
@@ -302,8 +267,8 @@ const ExamMetadataEditor: React.FC<Props> = ({
               </label>
               <Input
                 type="url"
-                value={masterSolution}
-                onChange={e => setMasterSolution(e.currentTarget.value)}
+                value={draftState.master_solution}
+                onChange={e => setKey("master_solution", e.currentTarget.value)}
               />
             </FormGroup>
           </Col>
@@ -331,18 +296,18 @@ const ExamMetadataEditor: React.FC<Props> = ({
               <label className="form-input-label">Remark</label>
               <TextareaField
                 textareaProps={{
-                  onChange: e => setRemark(e.currentTarget.value),
+                  onChange: e => setKey("remark", e.currentTarget.value),
                 }}
               >
-                {remark}
+                {draftState.remark}
               </TextareaField>
             </FormGroup>
           </Col>
         </Row>
         <h6>Attachments</h6>
         <AttachmentsEditor
-          attachments={attachments}
-          setAttachments={setAttachments}
+          attachments={draftState.attachments}
+          setAttachments={a => setKey("attachments", a)}
         />
       </CardBody>
       <CardFooter>
