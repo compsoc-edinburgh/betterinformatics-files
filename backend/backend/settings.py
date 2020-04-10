211a7223ba4908d72622a7181e127d5f393657e7
lmoeller <lmoeller@student.ethz.ch>
2020-04-10 11:44:27 +0200

    Fix CSP

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ if DEBUG:
 else:
     allowed = ['https://{}/static/'.format(host) for host in REAL_ALLOWED_HOSTS]
     CSP_SCRIPT_SRC = ("'unsafe-eval'", *allowed)
-CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "https://fonts.googleapis.com")
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://static.vseth.ethz.ch")
 CSP_FONT_SRC = ("'self'", "https://fonts.gstatic.com")
 CSP_CONNECT_SRC = ("'self'", "https://static.vseth.ethz.ch")
 CSP_IMG_SRC = ("'self'", "data:", "https://static.vis.ethz.ch", "https://static.vseth.ethz.ch")
