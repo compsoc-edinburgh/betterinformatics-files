170a99b9e0872de0e796e958c7c1b0a8ae089c06
Benjamin Schmid <git@schmidb.ch>
2020-04-13 14:30:18 +0200

    remove health endpoint from debug middleware

--- backend/backend/debugging.py
+++ backend/backend/debugging.py
@@ -10,12 +10,13 @@ def db_profiling_middleware(get_response):
         start = time.time()
         response = get_response(request)
         end = time.time()
-        logging.info('Request to %s took %s ms with %s queries.', request.get_full_path(), (end - start) * 1000, len(connection.queries))
+        if request.get_full_path() != '/health/':
+            logging.info('Request to %s took %s ms with %s queries.', request.get_full_path(), (end - start) * 1000, len(connection.queries))
 
-        if settings.DEBUG:
-            if len(connection.queries) > 20:
-                for query in connection.queries[:20]:
-                    logging.info(query)
+            if settings.DEBUG:
+                if len(connection.queries) > 20:
+                    for query in connection.queries[:20]:
+                        logging.info(query)
 
         return response
     return middleware
