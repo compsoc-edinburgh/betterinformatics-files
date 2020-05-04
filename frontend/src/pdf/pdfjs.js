fd1e3c8c7d78e911a5d1b954659e0c739d0a0430
lmoeller <lmoeller@student.ethz.ch>
2020-05-04 18:31:49 +0200

    Add comment about reexporting pdf-js

--- frontend/src/pdf/pdfjs.js
+++ frontend/src/pdf/pdfjs.js
@@ -1,3 +1,9 @@
+/**
+ * By default worker-loader won't output to /static. Therefore we need to reexport
+ * pdfjs and inject the correct location using webpack's loader sytnax. I hope this
+ * is just a temporary workaround because there probably is a way to do this nicely.
+ * We could also overwrite the webpack config...
+ */
 var pdfjs = require("pdfjs-dist/build/pdf.js");
 // eslint-disable-next-line import/no-webpack-loader-syntax
 var PdfjsWorker = require("worker-loader?name=static/workers/[hash].worker.js!pdfjs-dist/build/pdf.worker.js");
