64aab4f6dd8f7f2f34d01077d04a12d559a09182
Lukas Möller <lmoeller@student.ethz.ch>
2020-06-03 19:25:01 +0200

    Fix layout issue on mobile devices

--- frontend/src/components/exam-grid.tsx
+++ frontend/src/components/exam-grid.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const gridStyles = css`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(420px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-column-gap: 9px;
   grid-row-gap: 9px;
 `;
