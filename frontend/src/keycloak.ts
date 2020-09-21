90cd5dafffd470ddb1ddcc6215454330e4175d31
lmoeller <lmoeller@student.ethz.ch>
2020-09-21 16:39:28 +0200

    Remove console.log from keycloak

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -7,7 +7,6 @@ const getKeycloakInstance = () => {
     throw new Error("Context data container is not a script element.");
   const text = element.innerText;
   const keycloakInstanceConfig = JSON.parse(text).keycloak;
-  console.log(keycloakInstanceConfig);
   const keycloak = Keycloak(keycloakInstanceConfig);
   return keycloak;
 };
