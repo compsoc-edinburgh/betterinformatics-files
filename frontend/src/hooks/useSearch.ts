9244eadb375d02ef05cc0caeef03b2f8fbe01e5f
lmoeller <lmoeller@student.ethz.ch>
2020-10-19 20:37:22 +0200

    Add jsdoc to useSearch

--- frontend/src/hooks/useSearch.ts
+++ frontend/src/hooks/useSearch.ts
@@ -1,6 +1,14 @@
 import Fuse from "fuse.js";
 import { useMemo } from "react";
 
+/**
+ * A React wrapper around the fuse.js search method that returns all data source elements
+ * if pattern is undefined.
+ * @param data The array that should be used as the fuse.js data source
+ * @param options An object of fuse.js options
+ * @param pattern The pattern that should be searched for
+ * @param searchOptions Options passed to the fuse.js search method
+ */
 const useSearch = <T>(
   data: T[],
 
