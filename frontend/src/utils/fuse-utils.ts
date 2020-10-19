ca6106cbcb264b4b2698c657fe2d69095aab1363
lmoeller <lmoeller@student.ethz.ch>
2020-10-19 20:43:15 +0200

    Add comments in fuse-utils

--- frontend/src/utils/fuse-utils.ts
+++ frontend/src/utils/fuse-utils.ts
@@ -1,9 +1,21 @@
 import Fuse from "fuse.js";
 
+/**
+ * Returns The RangeTuple with the specified key if it was found in `result`
+ * @param result The fuse.js search result
+ * @param key The key that should be found
+ */
 export const getMatch = <T>(result: Fuse.FuseResult<T>, key: string) => {
   return result.matches?.find(e => e.key === key)?.indices;
 };
 
+/**
+ * Transforms a RangeTuple[] into an array of [string, boolean] where the string
+ * is a substring of value, the boolean specifies whether that substring is part of
+ * the match and the strings in the array partition the string `value`.
+ * @param m the  fuse.js match array that should be processed
+ * @param value the value that `m` matched against
+ */
 export const processMatch = (
   m: readonly Fuse.RangeTuple[] | undefined,
   value: string,
