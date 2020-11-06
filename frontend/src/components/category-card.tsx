5a5cb6a658d79e140b88e16538267c36b78367bc
lmoeller <lmoeller@student.ethz.ch>
2020-11-06 10:40:35 +0100

    Fix category card

--- frontend/src/components/category-card.tsx
+++ frontend/src/components/category-card.tsx
@@ -1,60 +1,37 @@
 import { Card, CardBody, CardFooter, Progress } from "@vseth/components";
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { SearchResult } from "../hooks/useSearch";
 import { CategoryMetaData } from "../interfaces";
+import { hl } from "../utils/search-utils";
 import { focusOutline } from "../utils/style";
-import Fuse from "fuse.js";
-import { useMemo } from "react";
-import { css } from "emotion";
-import { processMatch, getMatch } from "../utils/fuse-utils";
-
-const highlighted = css`
-  background-color: var(--yellow);
-`;
 
 interface Props {
-  category: Fuse.FuseResult<CategoryMetaData>;
+  category: SearchResult<CategoryMetaData>;
 }
 const CategoryCard: React.FC<Props> = ({ category }) => {
   const history = useHistory();
-  const m = useMemo(
-    () =>
-      processMatch(
-        getMatch(category, "displayname"),
-        category.item.displayname,
-      ),
-    [category],
-  );
   const handleKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
     if (e.keyCode === 13) {
-      history.push(`/category/${category.item.slug}`);
+      history.push(`/category/${category.slug}`);
     }
   };
   return (
     <Card
       className={focusOutline}
       tabIndex={0}
-      onClick={() => history.push(`/category/${category.item.slug}`)}
+      onClick={() => history.push(`/category/${category.slug}`)}
       onKeyDown={handleKeyDown}
     >
       <CardBody>
-        <h5>
-          {m.map(([str, isMatch], i) => (
-            <span key={i} className={isMatch ? highlighted : undefined}>
-              {str}
-            </span>
-          ))}
-        </h5>
+        <h5>{hl(category.displayname, category.match)}</h5>
         <div>
-          Exams:{" "}
-          {`${category.item.examcountanswered} / ${category.item.examcountpublic}`}
+          Exams: {`${category.examcountanswered} / ${category.examcountpublic}`}
         </div>
-        <div>
-          Answers: {((category.item.answerprogress * 100) | 0).toString()} %
-        </div>
+        <div>Answers: {((category.answerprogress * 100) | 0).toString()} %</div>
       </CardBody>
       <CardFooter>
-        <Progress value={category.item.answerprogress} max={1} />
+        <Progress value={category.answerprogress} max={1} />
       </CardFooter>
     </Card>
   );
