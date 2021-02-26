5347b287673f9de3bad124c783d99659628a3da1
Lukas Möller <lmoeller@student.ethz.ch>
2021-02-26 15:30:44 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,7 +130,7 @@ CSP_CONNECT_SRC = (
     "'self'",
     "https://static.vseth.ethz.ch",
     "https://auth.vseth.ethz.ch",
-    "https://" + os.environ["SIP_S3_FILES_HOST"]
+    "https://" + os.environ.get("SIP_S3_FILES_HOST", "minio")
 )
 CSP_IMG_SRC = ("'self'", "data:", "https://static.vseth.ethz.ch")
 
