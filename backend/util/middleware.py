0518c1677374b1743d67efa7dbbb89e4d7eef482
lmoeller <lmoeller@student.ethz.ch>
2021-03-27 15:50:51 +0100

    Allow PUT requests to use files Modifies the parsing middleware so that multipart form requests set the FORM property of the request set correctly.

--- backend/util/middleware.py
+++ backend/util/middleware.py
@@ -5,14 +5,20 @@ from django.http.multipartparser import MultiPartParser
 
 def parse_request_middleware(get_response):
     def middleware(request):
-        if request.method == 'PUT':
+        if request.method == "PUT":
             try:
-                parser = MultiPartParser(request.META, BytesIO(request.body), request.upload_handlers)
+                parser = MultiPartParser(
+                    request.META, BytesIO(request.body), request.upload_handlers
+                )
+                _, files = request.parse_file_upload(request.META, request)
+                request.FILES.update(files)
                 request.DATA, _ = parser.parse()
             except Exception as e:
                 import traceback
+
                 traceback.print_exc()
-        elif request.method == 'POST':
+        elif request.method == "POST":
             request.DATA = request.POST
         return get_response(request)
+
     return middleware
