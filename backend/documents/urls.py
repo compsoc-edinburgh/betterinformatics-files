207ec89fdd0581c18685f5d0ca6e669d4598048e
lmoeller <lmoeller@student.ethz.ch>
2021-04-16 18:18:23 +0200

    Treat document slugs as str

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -8,27 +8,27 @@ urlpatterns = [
     path("file/<filename>", views.get_document_file, name="file"),
     path("", views.DocumentRootView.as_view(), name="root"),
     path(
-        "<str:username>/<slug:slug>/",
+        "<str:username>/<str:slug>/",
         views.DocumentElementView.as_view(),
         name="element",
     ),
     path(
-        "<str:username>/<slug:document_slug>/comments/",
+        "<str:username>/<str:document_slug>/comments/",
         views.DocumentCommentRootView.as_view(),
         name="comments_root",
     ),
     path(
-        "<str:username>/<slug:document_slug>/comments/<int:id>/",
+        "<str:username>/<str:document_slug>/comments/<int:id>/",
         views.DocumentCommentElementView.as_view(),
         name="comments_element",
     ),
     path(
-        "<str:username>/<slug:document_slug>/files/",
+        "<str:username>/<str:document_slug>/files/",
         views.DocumentFileRootView.as_view(),
         name="files_root",
     ),
     path(
-        "<str:username>/<slug:document_slug>/files/<int:id>/",
+        "<str:username>/<str:document_slug>/files/<int:id>/",
         views.DocumentFileElementView.as_view(),
         name="files_element",
     ),
