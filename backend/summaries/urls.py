44894de903a192a54952a7ebb8f5e4bf82daeb9b
lmoeller <lmoeller@student.ethz.ch>
2021-04-16 17:20:46 +0200

    Use str parser for summary urls

--- backend/summaries/urls.py
+++ backend/summaries/urls.py
@@ -8,27 +8,27 @@ urlpatterns = [
     path("file/<filename>", views.get_summary_file, name="file"),
     path("", views.SummaryRootView.as_view(), name="root"),
     path(
-        "<str:username>/<slug:slug>/",
+        "<str:username>/<str:slug>/",
         views.SummaryElementView.as_view(),
         name="element",
     ),
     path(
-        "<str:username>/<slug:summary_slug>/comments/",
+        "<str:username>/<str:summary_slug>/comments/",
         views.SummaryCommentRootView.as_view(),
         name="comments_root",
     ),
     path(
-        "<str:username>/<slug:summary_slug>/comments/<int:id>/",
+        "<str:username>/<str:summary_slug>/comments/<int:id>/",
         views.SummaryCommentElementView.as_view(),
         name="comments_element",
     ),
     path(
-        "<str:username>/<slug:summary_slug>/files/",
+        "<str:username>/<str:summary_slug>/files/",
         views.SummaryFileRootView.as_view(),
         name="files_root",
     ),
     path(
-        "<str:username>/<slug:summary_slug>/files/<int:id>/",
+        "<str:username>/<str:summary_slug>/files/<int:id>/",
         views.SummaryFileElementView.as_view(),
         name="files_element",
     ),
