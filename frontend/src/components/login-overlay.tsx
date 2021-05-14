9beb95a900f00b2a464f711a657c66f1423447e2
lmoeller <lmoeller@student.ethz.ch>
2021-05-14 14:03:22 +0200

    Add blob

--- frontend/src/components/login-overlay.tsx
+++ frontend/src/components/login-overlay.tsx
@@ -1,14 +1,13 @@
 import { useKeycloak } from "@react-keycloak/web";
 import { Button } from "@vseth/components";
 import React from "react";
-import IconButton from "./icon-button";
 
 const LoginOverlay: React.FC<{}> = () => {
   const { keycloak, initialized } = useKeycloak();
   return (
-    <>
-      <div className="py-4 text-center">
-        <div className="my-1 font-weight-bold">Please Sign in</div>
+    <div className="text-center position-cover d-flex align-items-center justify-content-center">
+      <div>
+        <h4 className="mb-4 font-weight-bold text-white">Please Sign in</h4>
         <Button
           size="lg"
           color="primary"
@@ -18,7 +17,7 @@ const LoginOverlay: React.FC<{}> = () => {
           Sign in with AAI
         </Button>
       </div>
-    </>
+    </div>
   );
 };
 export default LoginOverlay;
