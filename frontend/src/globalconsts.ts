5315f49f2544d29f8ca71f240082767efcb73ed4
jsteinmann <julian.steinmann@inf.ethz.ch>
2022-10-25 14:32:45 +0000

    Remove old SVGs and tutorial screenshots

--- frontend/src/globalconsts.ts
+++ frontend/src/globalconsts.ts
@@ -1,8 +1,6 @@
 enum ZIndex {
   imageOverlay = 42,
   panel = 100,
-  tutorialSlideShow,
-  tutorialSlideShowOverlayArea,
 }
 export default class GlobalConsts {
   static readonly momentParseString = "YYYY-MM-DDTHH:mm:ss.SSSSSSZZ";
