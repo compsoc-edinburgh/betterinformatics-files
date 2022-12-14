77ad27f218d32048f2cc600f12859fe076162bdf
Jacques Hoffmann <jhoffmann@student.ethz.ch>
2022-12-15 00:23:47 +0100

    Small Change

--- frontend/src/components/Editor/EditorHelp.tsx
+++ frontend/src/components/Editor/EditorHelp.tsx
@@ -39,19 +39,25 @@ Note that you can either upload images by copy-pasting or using the upload tool.
 
 Math is rendered using the \`react-katex\` package, so it fully supports the [Katex](https://github.com/KaTeX/KaTeX) specification. See the **list of [supported functions](https://katex.org/docs/supported.html)** to get all the details. Here are the most important points.
 * Most standard operations should be supported.
-* You can do inline math, and block math with \`$...$\` and \`$$...$$\` respectively.
+* You can do inline math, and block math with \`$...$\` and \`$$...$$\` respectively. Note that block math requires a new lines: \`$$\\n <your-math> \\n$$\`.
 * Even align blocks are fully supported!
 * Persistent macros can be used throughout one post. See below for more info!
 
-As an example, \`$\\int_a^b \\sin x \\,dx$\` generates the following math expression: $\\int_a^b \\sin x \\,dx$.
+As an example, \`$\\int_a^b \\sin x \\, dx$\` generates the following math expression: $\\int_a^b \\sin x \\,dx$, and as block math:
+$$
+\\int_a^b \\sin x \\, dx
+$$
 
+##### Persistent Macros
 $\\gdef\\comment#1{}$
 $\\gdef\\cent{\\char"00A2}$
 $\\comment{PERSISTENT MACRO DEFINITION IS JUST ABOVE HERE! :)}$
 **Macros are persistent throughout a post** using \`$\\gdef...$\`.
 Declare any commands you may need at the top of your answer and use them in other Math Blocks throughout your answer.
-This is often useful in case you need a symbol that isn't predefined in Katex.
-For example, use \`\\gdef\\cent{\\char"00A2}\` to define the $\\cent$-symbol once at the top. Now we can use \`$\\cent$\` to generate $\\cent$.
+
+This is often useful in case you need a symbol that isn't predefined in Katex. Just write these definitions once at the top and use the commands multiple times in different blocks.
+* The "diff" symbol in integrals, that can be defined like this: \`$\\gdef\\diff{\\mathop{}\\!\\mathrm{d}}$\`. You can then use it in integrals: $\\int_a^b \\sin x \\diff x$. Probably the most widely used example!
+* The "cent" symbol: \`\\gdef\\cent{\\char"00A2}\`. Now use \`$\\cent$\` in the post to get $\\cent$.
 
 Use source mode to see what is hidden in this answer and for one more example to define a \`\\comment\` command!
 $\\comment{Comment is a persistent macro and is available as command throughout the post!}$
