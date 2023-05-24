82b29176aa7c5606672c839d3727f545f2af86bb
Thomas Gassmann <tgassmann@student.ethz.ch>
2023-05-25 00:48:53 +0200

    Truncate text in category badge if too long
    Signed-off-by: Thomas Gassmann <tgassmann@student.ethz.ch>

--- frontend/src/components/document-card.tsx
+++ frontend/src/components/document-card.tsx
@@ -9,11 +9,17 @@ import {
   LikeIcon,
 } from "@vseth/components";
 import { Link } from "react-router-dom";
+import { css, cx } from "@emotion/css";
 
 interface DocumentCardProps {
   document: Document;
   showCategory?: boolean;
 }
+
+const overflowHiddenStyles = css`
+  overflow: hidden;
+`;
+
 const DocumentCard: React.FC<DocumentCardProps> = ({
   document,
   showCategory,
@@ -27,22 +33,27 @@ const DocumentCard: React.FC<DocumentCardProps> = ({
         >
           <CardTitle tag="h6">{document.display_name}</CardTitle>
         </Link>
-        <div>
+        <div className="d-flex align-items-center">
           <Link to={`/user/${document.author}`} className="text-muted">
             @{document.author}
           </Link>
-          {document.liked ? (
-            <span className="text-danger ml-2">
-              <LikeFilledIcon className="mr-1" /> {document.like_count}
-            </span>
-          ) : (
-            <span className="text-muted ml-2">
-              <LikeIcon className="mr-1" /> {document.like_count}
-            </span>
-          )}
+          <div>
+            {document.liked ? (
+              <span className="text-danger ml-2 d-flex align-items-center">
+                <LikeFilledIcon className="mr-1" /> {document.like_count}
+              </span>
+            ) : (
+              <span className="text-muted ml-2 d-flex align-items-center">
+                <LikeIcon className="mr-1" /> {document.like_count}
+              </span>
+            )}
+          </div>
           {showCategory && (
-            <Badge className="ml-2">
-              <Link to={`/category/${document.category}`}>
+            <Badge className={cx(overflowHiddenStyles, "ml-2")}>
+              <Link
+                className="align-middle d-inline-block text-truncate mw-100"
+                to={`/category/${document.category}`}
+              >
                 {document.category_display_name}
               </Link>
             </Badge>
