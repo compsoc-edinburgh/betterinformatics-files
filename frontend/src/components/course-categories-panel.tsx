33f47ab0a46e75e3657b13ec87ee923f9eada9e1
Georg Ye <calenwuc@gmail.com>
2023-10-10 17:45:27 +0200

    changed to cursor-pointer

--- frontend/src/components/course-categories-panel.tsx
+++ frontend/src/components/course-categories-panel.tsx
@@ -3,12 +3,28 @@ import React, { useCallback } from "react";
 import Panel from "./panel-left";
 import { CategoryMetaData } from "../interfaces";
 
-const paginationStyle = css`
-  & .pagination {
-    display: inline-block;
+const categoriesHeaderStyle = css`
+  & .categories-header-link {
+    cursor: pointer;
+    color: #000000;
+    transition: color 0.3s;
+    margin-top: 1.2rem;
+    margin-bottom: 1.2rem;
   }
-  & .pagination .page-item {
-    display: inline-block;
+  & .categories-header-link:hover {
+    color: #080808;
+  }
+`;
+
+const categoriesInnerStyle = css`
+  & .categories-inner-link {
+    cursor: pointer;
+    color: #000000;
+    transition: color 0.3s;
+    margin-bottom: 0.6rem;
+  }
+  & .categories-inner-link:hover {
+    color: #080808;
   }
 `;
 
@@ -56,12 +72,17 @@ const CourseCategoriesPanel: React.FC<CourseCategoriesPanelProps> = ({
       {metaList &&
         metaList.map(([meta1display, meta2]) => (
           <div key={meta1display}>
-            <h5 className="my-3" onClick={() => scrollToDiv(slugify(meta1display))}>
+            <h5 className='my-3'
+              style={{
+                cursor: 'pointer',
+              }} onClick={() => scrollToDiv(slugify(meta1display))}>
               {meta1display}
             </h5>
             {meta2.map(([meta2display, categories]) => (
               <div key={meta2display}>
-                <p className="mb-2"  onClick={() => scrollToDiv(slugify(meta1display) + slugify(meta2display))}>
+                <p className='mb-2' style={{
+                    cursor: 'pointer',
+                  }} onClick={() => scrollToDiv(slugify(meta1display) + slugify(meta2display))}>
                   {meta2display}
                 </p>
               </div>
