53893cca747b92e74a22d6b157a5bcb2db872f92
Yuto Takano <moa17stock@gmail.com>
2023-10-16 16:47:11 +0100

    Change main hero to say BetterInformatics Exam Collection

--- frontend/src/pages/login-page.tsx
+++ frontend/src/pages/login-page.tsx
@@ -10,29 +10,33 @@ const LoginPage: React.FC<{ isHome: boolean }> = ({ isHome = false }) => {
     <>
       <Container>
         <Row>
-          <Col md={5} className="d-flex flex-column justify-content-center">
-            <h1 className="mb-5 display-3">Community Solutions</h1>
+          <Col md={6} className="d-flex flex-column justify-content-center">
+            <h1 className="mb-5 display-3">
+              Better&shy;Informatics Exam Collection
+            </h1>
             <h6>
-              Community Solutions is a platform for students that allows them to
-              share answers of previous exams, comment on answers and upload
-              summaries.
+              The BI Exam Collection is a platform for students to share answers
+              for previous exams, leave comments on answers, and upload other
+              relevant documents.
             </h6>
           </Col>
           <Col
             xs={11}
-            md={7}
+            md={6}
             lg={6}
             xl={4}
             className="py-4 position-relative d-flex align-items-center mx-auto"
           >
             <svg
+            x="0px" y="0px"
               viewBox="0 0 200 200"
               xmlns="http://www.w3.org/2000/svg"
               className="text-primary m-auto w-100"
             >
               <path
-                d="M40.3,-68.2C51.6,-63.2,59.8,-51.2,65.1,-38.7C70.4,-26.1,72.9,-13.1,75.5,1.5C78.2,16.1,81,32.3,76.7,46.6C72.5,61,61.2,73.7,47.2,78.3C33.2,83,16.6,79.6,2.6,75.1C-11.4,70.6,-22.8,64.9,-36.1,59.9C-49.3,54.8,-64.5,50.4,-70.5,40.5C-76.6,30.6,-73.7,15.3,-70.5,1.8C-67.3,-11.6,-63.9,-23.3,-59.8,-36.7C-55.8,-50.1,-51.1,-65.3,-41,-71C-31,-76.7,-15.5,-72.9,-0.5,-72C14.5,-71.2,28.9,-73.2,40.3,-68.2Z"
-                transform="translate(100 100)"
+                d="M146.9,18.7c15.7,0.8,32.3,8.2,30.9,29.7c-1.4,22.2,7.7,33.5,7.4,68.9c-0.1,16.3-1.8,34.1-3.3,37
+                c-6.4,12.3-19,18.7-33.6,20.1c-8.2,0.8-19.9-2.5-33.9-7s-28.6,1.3-41.5,7.2c-12.9,6-43.9,4.6-49.9-5.3c-6.1-9.9,3.9-27.2,7-48.5
+                c2.4-16.4-7.7-42.7-2.9-64c8-36,31.3-35,35.6-35c5.6,0,24.7,4.1,39.7,4.3C128.4,26.5,134.5,18,146.9,18.7z"
               />
             </svg>
             <LoginOverlay />
