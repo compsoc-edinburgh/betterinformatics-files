4894371748e0fb17f67973d6be875a70c2c04231
Julian Steinmann <jsteinmann@ethz.ch>
2023-12-05 17:11:08 +0100

    Migrate use-query-params syntax to v2
    We keep using query-string for now.
    Could be dropped for their built-in solution later on.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,18 @@
 import * as ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ReactRouter5Adapter } from "use-query-params/adapters/react-router-5";
 import { QueryParamProvider } from "use-query-params";
 import App from "./app";
 import React from "react";
+import { parse, stringify } from "query-string";
 
 ReactDOM.render(
   <BrowserRouter>
-    <QueryParamProvider ReactRouterRoute={Route}>
+    <QueryParamProvider
+      adapter={ReactRouter5Adapter}
+      options={{ searchStringToObject: parse, objectToSearchString: stringify }}
+    >
       <App />
     </QueryParamProvider>
   </BrowserRouter>,
