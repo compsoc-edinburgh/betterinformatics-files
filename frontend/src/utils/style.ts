d9e697a1d667d238b0ec29c39a8db531a72bdb4d
Yuto Takano <y.takano@sms.ed.ac.uk>
2023-12-27 06:11:12 +0000

    Use hover effect for better UX in category/exam cards

--- frontend/src/utils/style.ts
+++ frontend/src/utils/style.ts
@@ -2,6 +2,11 @@ import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles(theme => ({
   focusOutline: {
+    transition: "box-shadow 150ms ease, transform 100ms ease",
+    "&:hover": {
+      boxShadow: theme.shadows.md,
+      transform: "scale(1.05)",
+    },
     "&:focus": {
       outlineWidth: 1.5,
       outlineStyle: "solid",
