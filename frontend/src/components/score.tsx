d3b06dbf40116bc5eabd810f70fbd5abd7d421de
jsteinmann <jsteinmann@ethz.ch>
2024-01-04 21:26:35 +0100

    Beautify up-/downvote component

--- frontend/src/components/score.tsx
+++ frontend/src/components/score.tsx
@@ -1,4 +1,4 @@
-import { Loader, Button } from "@mantine/core";
+import { Button, Paper } from "@mantine/core";
 import { useRequest } from "@umijs/hooks";
 import React from "react";
 import { Icon, ICONS } from "vseth-canine-ui";
@@ -30,38 +30,40 @@ const Score: React.FC<Props> = ({
     onSuccess: onSectionChanged,
   });
   return (
-    <Button.Group>
-      <TooltipButton
-        px={4}
-        tooltip="Downvote"
-        size="sm"
-        disabled={userVote === -1}
-        variant={userVote === -1 ? "outline" : "default"}
-        onClick={() => setLike(oid, -1)}
-      >
-        <Icon icon={ICONS.MINUS} size={18} />
-      </TooltipButton>
-      <TooltipButton
-        tooltip="Reset vote"
-        size="sm"
-        px="sm"
-        disabled={userVote === 0}
-        // variant="subtle"
-        onClick={() => setLike(oid, 0)}
-      >
-        {loading ? <Loader size="sm" /> : upvotes}
-      </TooltipButton>
-      <TooltipButton
-        px={4}
-        tooltip="Upvote"
-        size="sm"
-        disabled={userVote === 1}
-        variant={userVote === 1 ? "outline" : "default"}
-        onClick={() => setLike(oid, 1)}
-      >
-        <Icon icon={ICONS.PLUS} size={18} />
-      </TooltipButton>
-    </Button.Group>
+    <Paper shadow="xs">
+      <Button.Group>
+        <TooltipButton
+          px={8}
+          tooltip="Downvote"
+          size="sm"
+          disabled={userVote === -1}
+          style={{ borderRightWidth: 0, borderColor: "#ced4da" }}
+          onClick={() => setLike(oid, -1)}
+        >
+          <Icon icon={ICONS.DOWN} size={16} />
+        </TooltipButton>
+        <TooltipButton
+          tooltip="Reset vote"
+          size="sm"
+          px="sm"
+          miw={40}
+          loading={loading}
+          onClick={() => setLike(oid, 0)}
+        >
+          {upvotes}
+        </TooltipButton>
+        <TooltipButton
+          px={8}
+          tooltip="Upvote"
+          size="sm"
+          disabled={userVote === 1}
+          style={{ borderLeftWidth: 0, borderColor: "#ced4da" }}
+          onClick={() => setLike(oid, 1)}
+        >
+          <Icon icon={ICONS.UP} size={16} />
+        </TooltipButton>
+      </Button.Group>
+    </Paper>
   );
 };
 export default Score;
