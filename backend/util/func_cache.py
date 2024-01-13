f0581764186e6d705c2046a9bf46dd28bceb18fb
Yuto Takano <y.takano@sms.ed.ac.uk>
2024-01-13 04:19:22 +0000

    Disable cache in unit tests

--- backend/util/func_cache.py
+++ backend/util/func_cache.py
@@ -1,17 +1,21 @@
 import time
 
+from django.conf import settings
+
 
 def cache(validity):
-
     class FuncCache:
-
         def __init__(self, fun):
             self.fun = fun
             self.cache = {}
             self.cache_time = {}
 
         def __call__(self, *item):
-            if item not in self.cache or time.time() - self.cache_time[item] > validity:
+            if (
+                settings.TESTING
+                or item not in self.cache
+                or time.time() - self.cache_time[item] > validity
+            ):
                 self.cache[item] = self.fun(*item)
                 self.cache_time[item] = time.time()
             return self.cache[item]
@@ -24,17 +28,19 @@ def cache(validity):
     return FuncCache
 
 
-def static_cache(validity): #Ignores function arguments
-
+def static_cache(validity):  # Ignores function arguments
     class FuncStaticCache:
-
         def __init__(self, fun):
             self.fun = fun
             self.cache = None
             self.cache_time = 0
 
         def __call__(self, *item):
-            if self.cache is None or time.time() - self.cache_time > validity:
+            if (
+                settings.TESTING
+                or self.cache is None
+                or time.time() - self.cache_time > validity
+            ):
                 self.cache = self.fun(*item)
                 self.cache_time = time.time()
             return self.cache
