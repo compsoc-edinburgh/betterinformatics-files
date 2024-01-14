compsoc-edinburgh/betterinformatics-files

commit 8aefbea8fa83e0d6ec5cc01b9dfd9cf20614922f

Display the category semester using semesterOptions mapping
Previously the semester field on a category page just displayed whatever
was returned from the API response. We change this to display a
human-readable string using a mapping that was already defined in the
metadata editor component.

frontend/src/pages/category-page.tsx
@@ -23,7 +23,7 @@ import {
   useRemoveCategory,
 } from "../api/hooks";
 import { UserContext, useUser } from "../auth";
-import CategoryMetaDataEditor from "../components/category-metadata-editor";
+import CategoryMetaDataEditor, { semesterOptions } from "../components/category-metadata-editor";
 import ExamList from "../components/exam-list";
 import LoadingOverlay from "../components/loading-overlay";
 import DocumentList from "../components/document-list";
@@ -152,7 +152,12 @@ const CategoryPageContent: React.FC<CategoryPageContentProps> = ({
           <Grid mb="xs">
             {metaData.semester && (
               <Grid.Col span="content">
-                Semester: <Badge>{metaData.semester}</Badge>
+                Semester:{" "}
+                <Badge>
+                  {semesterOptions[
+                    metaData.semester as keyof typeof semesterOptions
+                  ] ?? metaData.semester}
+                </Badge>
               </Grid.Col>
             )}
             {metaData.form && (

frontend/src/components/category-metadata-editor.tsx
@@ -182,7 +182,7 @@ const applyChanges = async (
 
 // These values are hardcoded in the backend database model, so you must perform
 // database migrations when modifing them.
-const semesterOptions = createOptions({
+export const semesterOptions = createOptions({
   none: "--",
   sem1: "Semester 1",
   sem2: "Semester 2",

commit 20b7164c8770b9ed48a797ba2af006b30d159568

Fix semester display error introduced in 8aefbea due to incorrect access to semesterOptions

frontend/src/pages/category-page.tsx
@@ -156,7 +156,7 @@ const CategoryPageContent: React.FC<CategoryPageContentProps> = ({
                 <Badge>
                   {semesterOptions[
                     metaData.semester as keyof typeof semesterOptions
-                  ] ?? metaData.semester}
+                  ]?.label ?? metaData.semester}
                 </Badge>
               </Grid.Col>
             )}