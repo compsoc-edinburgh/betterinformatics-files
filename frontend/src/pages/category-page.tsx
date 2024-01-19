compsoc-edinburgh/betterinformatics-files

commit 96a5d81815a9c800205c6ce7f0dd321a38f6da1c

Show shadow course data on category page as well

frontend/src/pages/category-page.tsx
@@ -103,10 +103,16 @@ const CategoryPageContent: React.FC<CategoryPageContentProps> = ({
   const asynchronously_updated_euclid_codes = useMemo(() => {
     // While the BI course JSON is loading, it is just a list of codes with
     // no BI course data
-    if (bi_courses_loading || !bi_courses_data) return metaData.euclid_codes.map(c => [c, undefined] as const);
+    if (bi_courses_loading || !bi_courses_data) {
+      return metaData.euclid_codes.map(c => [c, undefined, undefined] as const);
+    }
 
     // Once data is ready, it becomes a list of codes and BI course data
-    return metaData.euclid_codes.map(c => [c, Object.values(bi_courses_data.list).find(d => d.euclid_code === c)] as const);
+    return metaData.euclid_codes.map(c => [
+      c,
+      Object.values(bi_courses_data.list).find(d => d.euclid_code === c),
+      Object.values(bi_courses_data.list).find(d => d.euclid_code_shadow === c),
+    ] as const);
   }, [metaData, bi_courses_loading, bi_courses_data]);
   return (
     <>
@@ -162,18 +168,19 @@ const CategoryPageContent: React.FC<CategoryPageContentProps> = ({
           </Flex>
 
           <List>
-            Offered as:
-            {asynchronously_updated_euclid_codes.map(([code, bi_data]) => (
+            {asynchronously_updated_euclid_codes.length > 0 && "Offered as:"}
+            {asynchronously_updated_euclid_codes.map(([code, bi_data, shadow_data]) => (
               <List.Item key={code}>
                 <Group>
                   <Badge
                     // Will show red if data isn't available or still loading
-                    color={bi_data ? "violet" : "red"}
+                    color={bi_data || shadow_data ? "violet" : "red"}
                     radius="xs"
                     component="a"
-                    href={bi_data?.course_url}
+                    // Choose course URL over EUCLID URL
+                    href={bi_data?.course_url ?? bi_data?.euclid_url ?? shadow_data?.course_url ?? shadow_data?.euclid_url_shadow}
                     // Badges don't look clickable by default, use pointer
-                    styles={{ inner: { cursor: bi_data ? "pointer" : "default" } }}
+                    styles={{ inner: { cursor: bi_data || shadow_data ? "pointer" : "default" } }}
                   >
                     {code}
                   </Badge>
@@ -181,7 +188,10 @@ const CategoryPageContent: React.FC<CategoryPageContentProps> = ({
                   {!bi_courses_loading && bi_data && (
                     `${bi_data.name} (SCQF ${bi_data.level}, Semester ${bi_data.delivery_ordinal})`
                   )}
-                  {!bi_courses_loading && !bi_data && "Not Running"}
+                  {!bi_courses_loading && shadow_data && (
+                    `${shadow_data.name} (Shadow of ${shadow_data.euclid_code})`
+                  )}
+                  {!bi_courses_loading && !bi_data && !shadow_data && "Not Running"}
                   {bi_courses_error && bi_courses_error.message}
                   {bi_courses_loading && <Skeleton height="1rem" width="8rem" />}
                 </Group>

frontend/src/api/hooks.ts
@@ -3,7 +3,7 @@ import { PDFDocumentProxy } from "pdfjs-dist/types/src/display/api";
 import {
   Answer,
   AnswerSection,
-  BICourseList,
+  BICourseDict,
   CategoryExam,
   CategoryMetaData,
   CategoryMetaDataMinimal,
@@ -37,15 +37,20 @@ const loadUserInfo = async (username: string) => {
 };
 
 export const useUserInfo = (username: string) => {
-  const { error, loading, data, run } = useRequest(() => loadUserInfo(username), {
-    refreshDeps: [username],
-    cacheKey: `userinfo-${username}`,
-  });
-  return [error, loading, data, run ] as const;
+  const { error, loading, data, run } = useRequest(
+    () => loadUserInfo(username),
+    {
+      refreshDeps: [username],
+      cacheKey: `userinfo-${username}`,
+    },
+  );
+  return [error, loading, data, run] as const;
 };
 const setUserDisplayUsername = async (displayUsername: string) => {
-  await fetchPost("/api/auth/update_name/", { display_username: displayUsername });
-}
+  await fetchPost("/api/auth/update_name/", {
+    display_username: displayUsername,
+  });
+};
 export const useSetUserDisplayUsername = (onSuccess: () => void) => {
   const { error, loading, run } = useRequest(setUserDisplayUsername, {
     manual: true,
@@ -57,15 +62,17 @@ export const useSetUserDisplayUsername = (onSuccess: () => void) => {
 const loadBICourseList = async () => {
   // Use normal fetch() instead of fetchGet as the latter is for internal API
   // and sends some credential cookies
-  return ((await fetch("https://betterinformatics.com/courses.json")).json()) as Promise<BICourseList>;
-}
+  return fetch("https://betterinformatics.com/courses.json")
+    .then(r => r.json())
+    .then(r => r?.list) as Promise<BICourseDict>;
+};
 
 export const useBICourseList = () => {
   const { error, loading, data } = useRequest(loadBICourseList, {
     cacheKey: "bicourselist",
   });
   return [error, loading, data] as const;
-}
+};
 
 const loadEnabledNotifications = async (isMyself: boolean) => {
   if (isMyself) {

commit 832cde7c7de4b5b1077b512f961ab7403add4a9a

Fix critical frontend bug introduced by 96a5d81
Bug was that the category page attempted to access .list of a
BICourseDict because I forgot to change that when modifying the return
value of the useBICourses hook from BICourseList to BICourseDict.

frontend/src/pages/category-page.tsx
@@ -110,8 +110,8 @@ const CategoryPageContent: React.FC<CategoryPageContentProps> = ({
     // Once data is ready, it becomes a list of codes and BI course data
     return metaData.euclid_codes.map(c => [
       c,
-      Object.values(bi_courses_data.list).find(d => d.euclid_code === c),
-      Object.values(bi_courses_data.list).find(d => d.euclid_code_shadow === c),
+      Object.values(bi_courses_data).find(d => d.euclid_code === c),
+      Object.values(bi_courses_data).find(d => d.euclid_code_shadow === c),
     ] as const);
   }, [metaData, bi_courses_loading, bi_courses_data]);
   return (