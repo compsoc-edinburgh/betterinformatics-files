5658d452b6b5ffd2e8f572465719f99a5592dbaa
Leandro Zazzi <leandro.zazzi@inf.ethz.ch>
2024-04-23 20:38:25 +0200

    custom Markdown for official solutions

---
+++ frontend/src/components/OfficialSolution.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { PDFDocumentProxy } from 'pdfjs-dist';
+import PDF from '../pdf/pdf-renderer';
+
+
+export default function OfficialSolution(){
+  return "TEST OFFICIAL MD" 
+}
+
