d4be3158076e598b322bffa328686bcfbe3021d4
Leandro Zazzi <leandro.zazzi@inf.ethz.ch>
2024-04-24 09:13:32 +0200

    try rendering a pdf.

--- frontend/src/components/OfficialSolution.tsx
+++ frontend/src/components/OfficialSolution.tsx
@@ -1,9 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
 import { PDFDocumentProxy } from 'pdfjs-dist';
 import PDF from '../pdf/pdf-renderer';
+import { getDocument } from '../pdf/pdfjs';
 
 
-export default function OfficialSolution(){
-  return "TEST OFFICIAL MD" 
+export default async function OfficialSolution(){
+
+    let pdfprox = getDocument({
+        url: "https://ethz.ch/content/dam/ethz/main/education/bachelor/studiengaenge/files/ETH-Zurich-Degree-programmes.pdf",
+        disableStream: true,
+        disableAutoFetch: true,
+      })
+    
+    let renderer = new PDF(pdfprox)
+    // Promise<[HTMLCanvasElement, boolean, PdfCanvasReference]
+    let [ret , bo, pdfcanv] = await renderer.renderCanvasSplit(1,1,0.4,0.8)
+    return ret
+
+  
 }
 
