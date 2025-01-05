4bb9f1b7ca32809caf510a80a34f4ec0012d7257
Yuto Takano <takanoy@student.ethz.ch>
2025-01-06 00:10:46 +0900

    Use legacy mode of pdfjs v4 for compatibility with iPad Safari and older browsers

--- frontend/src/pdf/pdfjs.js
+++ frontend/src/pdf/pdfjs.js
@@ -1,8 +1,8 @@
-import * as pdfjs from "pdfjs-dist";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.min.mjs?url";
+import * as pdfjs from "pdfjs-dist/legacy/build/pdf";
+import pdfjsWorker from "pdfjs-dist/legacy/build/pdf.worker.min.mjs?url";
 
 if (typeof window !== "undefined" && window.Worker) {
   pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 }
 
-export { getDocument } from "pdfjs-dist/build/pdf.mjs";
+export { getDocument } from "pdfjs-dist/legacy/build/pdf";
