91db4f531851dfc4dd11bbf9a6b837208697ea83
Burak Emir Sezen <burakemirsezen@gmail.com>
2025-05-03 21:14:11 +0200

    Change backend implementation to allow smarter caching

--- backend/scoreboard/views.py
+++ backend/scoreboard/views.py
@@ -4,14 +4,14 @@ from util import response, func_cache
 from myauth import auth_check
 from myauth.models import MyUser
 from django.shortcuts import get_object_or_404
-from django.db.models import Count, F, Q, Value as V
+from django.db.models import F, Q, Value as V
 from django.db.models.functions import Concat
 
 
 def get_user_scores(user, res):
     res.update(
         {
-            "rank": get_user_ranking(user),
+            "rank": get_ranking_list().index(user.username) + 1,
             "total_users": MyUser.objects.count(),
             "score": user.scores.document_likes + user.scores.upvotes - user.scores.downvotes,
             "score_answers": user.answer_set.filter(kind=Answer.Kind.PERSONAL).count(),
@@ -25,12 +25,12 @@ def get_user_scores(user, res):
     return res
 
 @func_cache.cache(600)
-def get_user_ranking(user):
+def get_ranking_list():
     return list(MyUser.objects.annotate(
         score=F("scores__document_likes")
         + F("scores__upvotes")
         - F("scores__downvotes"),
-    ).order_by("-score").values_list("username", flat=True)).index(user.username) + 1
+    ).order_by("-score").values_list("username", flat=True))
 
 @func_cache.cache(600)
 def get_scoreboard_top(scoretype, limit):
