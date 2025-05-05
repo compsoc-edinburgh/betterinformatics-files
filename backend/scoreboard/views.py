24f540e9f191f85050eb77ce0cd93b2eb23a4dff
Burak Emir Sezen <burakemirsezen@gmail.com>
2025-05-05 02:47:26 +0200

    Add a check to prevent a crash when a new user is not found in the cached list

--- backend/scoreboard/views.py
+++ backend/scoreboard/views.py
@@ -9,10 +9,16 @@ from django.db.models.functions import Concat
 
 
 def get_user_scores(user, res):
+    list = get_ranking_list()
+    total_users = MyUser.objects.count()
+    if user.username in list:
+        rank = list.index(user.username) + 1
+    else:
+        rank = total_users
     res.update(
         {
-            "rank": get_ranking_list().index(user.username) + 1,
-            "total_users": MyUser.objects.count(),
+            "rank": rank,
+            "total_users": total_users,
             "score": user.scores.document_likes + user.scores.upvotes - user.scores.downvotes,
             "score_answers": user.answer_set.filter(kind=Answer.Kind.PERSONAL).count(),
             "score_comments": user.answers_comments.count(),
