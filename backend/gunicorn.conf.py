b0a8a14ebaab2acf319f44c26b3bdc1ebcd64a31
Jacques Hoffmann <jhoffmann@student.ethz.ch>
2025-08-10 22:46:30 +0200

    Trace & Instrument backend

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-"""Django's command-line utility for administrative tasks."""
-
-import os
-import sys
-
 from opentelemetry.instrumentation.django import DjangoInstrumentor
 from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 
@@ -14,35 +8,13 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 
-import logging
-
-# Enable debug logging for OpenTelemetry
-logging.getLogger("opentelemetry").setLevel(logging.DEBUG)
-logging.basicConfig(level=logging.DEBUG)
-
-
-def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
 
     DjangoInstrumentor().instrument()
-
     resource = Resource.create(attributes={"service.name": "api-service"})
 
     trace.set_tracer_provider(TracerProvider(resource=resource))
     span_processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://alloy:4317"))
     trace.get_tracer_provider().add_span_processor(span_processor)
     Psycopg2Instrumentor().instrument()
-
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
