5ccfe173c8ed4029273b408fa2547daac8a7d4ee
Yuto Takano <takanoy@student.ethz.ch>
2025-08-17 23:39:44 +0200

    Add a unit test for searching with a category filter

--- backend/answers/test/tests_search.py
+++ backend/answers/test/tests_search.py
@@ -12,7 +12,8 @@ logger = logging.getLogger(__name__)
 
 
 class TestSearch(ComsolTestExamsData):
-    def test_search_page(self):
+    def mySetUp(self, call_my_setup=True):
+        """Upload a test PDF file and create a search vector for it."""
         location = join(dirname(__file__), "search_test.pdf")
         with open(location, "rb") as infile:
             filename = self.post(
@@ -20,13 +21,42 @@ class TestSearch(ComsolTestExamsData):
                 {"category": "default", "displayname": "Test", "file": infile},
             )["filename"]
             ExamPage.objects.update(search_vector=SearchVector("text"))
-            res = self.post("/api/exam/search/", {"term": "uniqueidthatwecansearch"})[
-                "value"
-            ]
-            self.assertEqual(len(res), 1)
-            match = res[0]
-            self.assertEqual(match["type"], "exam")
-            self.assertEqual(len(match["headline"]), 1)
-            self.assertEqual(match["category_displayname"], "default")
-            self.assertEqual(match["category_slug"], "default")
-            self.assertEqual(len(match["pages"]), 1)
+
+    def test_search_page(self):
+        """Test searching for a specific term in an exam."""
+        # First check that we get nothing if we don't include exams
+        res = self.post(
+            "/api/exam/search/",
+            {"term": "uniqueidthatwecansearch", "include_exams": False},
+        )["value"]
+        self.assertEqual(len(res), 0)
+
+        # Then check that we get a result if we include exams
+        res = self.post("/api/exam/search/", {"term": "uniqueidthatwecansearch"})[
+            "value"
+        ]
+        self.assertEqual(len(res), 1)
+
+        # Check that the result is what we expect
+        match = res[0]
+        self.assertEqual(match["type"], "exam")
+        self.assertEqual(len(match["headline"]), 1)
+        self.assertEqual(match["category_displayname"], "default")
+        self.assertEqual(match["category_slug"], "default")
+        self.assertEqual(len(match["pages"]), 1)
+
+    def test_search_with_category_filter(self):
+        """Test that searching with a category filter works as expected when
+        specifying a non-matching category vs a matching category.
+        """
+        res = self.post(
+            "/api/exam/search/",
+            {"term": "uniqueidthatwecansearch", "category": "not_default"},
+        )["value"]
+        self.assertEqual(len(res), 0)
+
+        res = self.post(
+            "/api/exam/search/",
+            {"term": "uniqueidthatwecansearch", "category": "default"},
+        )["value"]
+        self.assertEqual(len(res), 1)
