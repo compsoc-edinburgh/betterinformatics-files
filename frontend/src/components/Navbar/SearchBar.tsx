488ee47fc4d8356ab171f85765548ae06b9fc7ef
Yuto Takano <takanoy@student.ethz.ch>
2025-08-30 13:31:44 +0200

    Fix infinite re-render loop bug with useQuickSearchFilter

--- frontend/src/components/Navbar/SearchBar.tsx
+++ frontend/src/components/Navbar/SearchBar.tsx
@@ -296,17 +296,29 @@ export const QuickSearchFilterContext = createContext<{
  * @param displayname Used in UI to show the dropdown text in the quick-search bar
  */
 export const useQuickSearchFilter = (filter?: QuickSearchFilter) => {
-  const context = useContext(QuickSearchFilterContext);
+  const { filter: currentFilter, setFilter } = useContext(QuickSearchFilterContext) ?? {};
   useEffect(() => {
     // Context may be undefined if there is no SearchBarCategoryFilterContext in
     // the parent tree. If so, we won't do anything nor return cleanups.
-    if (!context) return;
+    if (!setFilter) return;
 
-    context.setFilter(filter);
+    // Prevent re-renders caused by passing a new object if the child values are
+    // the same. Otherwise, since the object passed to this hook is newly created
+    // every render, useEffect will trigger, setFilter will trigger, re-render
+    // is triggered by context value changing, which causes useEffect to trigger
+    // etc...
+    // The fix is to stop the recursion if our desired result is already set
+    if (filter?.slug === currentFilter?.slug) return;
+
+    setFilter(filter);
     return () => {
       // Cleanup function that runs whenever deps list changes or component
-      // unloads
-      context.setFilter(undefined);
+      // unloads. We have to do a check here as well that the desired state is
+      // not the current state, otherwise there will be an infinite loop where
+      // setFilter(undefined) will cause a context re-render, which will cause
+      // the hook to be unloaded, which calls useEffect's return callback, which
+      // sets setFilter(undefined) again.
+      if (currentFilter !== undefined) setFilter(undefined);
     }
-  }, [filter, context]);
+  }, [currentFilter, filter, setFilter]);
 }
