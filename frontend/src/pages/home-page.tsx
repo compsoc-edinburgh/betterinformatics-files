c3ee3571dcdd2959971b96d27e370689d9d604be
Yuto Takano <takanoy@student.ethz.ch>
2025-09-14 15:39:44 +0200

    Change home-page loader to be more subtle
    Solves the issue with two LoadingOverlay being laid on top of each other
    (one from HomePage while categories load, one from UserRouteContent while
    auth loads).
    
    Using a non-intrusive loader that doesn't cover the page also paves way
    for caching the categories and subtly hydrating them once the server responds.

--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -20,7 +20,6 @@ import { loadMetaCategories } from "../api/hooks";
 import { User, useUser } from "../auth";
 import CategoryCard from "../components/category-card";
 import Grid from "../components/grid";
-import LoadingOverlay from "../components/loading-overlay";
 import ContentContainer from "../components/secondary-container";
 import useSearch from "../hooks/useSearch";
 import useTitle from "../hooks/useTitle";
@@ -249,8 +248,8 @@ export const CategoryList: React.FC<{}> = () => {
         </Flex>
       </Container>
       <ContentContainer>
-        <LoadingOverlay visible={loading} />
-        <Container size="xl" py="md">
+        <Container size="xl" py="md" pos="relative">
+          {loading && !error && <Loader size="xs" color="gray" pos="absolute" top={0} right={0} />}
           {error ? (
             <Alert color="red">{error.toString()}</Alert>
           ) : mode === "alphabetical" || filter.length > 0 ? (
