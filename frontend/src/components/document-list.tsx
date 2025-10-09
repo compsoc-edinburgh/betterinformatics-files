84fe04dc66bfd8a1d2ed321cd42ecd91c3b93954
Zijun-Wan <nujiznaw8240@gmail.com>
2025-10-09 20:22:41 +0200

    changed label correspondingly

--- frontend/src/components/document-list.tsx
+++ frontend/src/components/document-list.tsx
@@ -79,7 +79,7 @@ const DocumentList: React.FC<Props> = ({ slug }) => {
       </Title>
       <Grid>
         <Paper withBorder shadow="md" style={{ minHeight: "6em" }}>
-          <Tooltip label="Add a new document">
+          <Tooltip label="Add Document Bundle">
             <Button
               style={{ width: "100%", height: "100%" }}
               onClick={open}
