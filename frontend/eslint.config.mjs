7a4d2463d97cdca2dcb7f0ce3e2264eb0dcd173f
Yuto Takano <y.takano@sms.ed.ac.uk>
2025-10-20 01:48:27 +0200

    Sort eslint rules, perform untyped import check on all files
    - Re-enable React rules since the plugin has been added back
      after flat-config migration
    - Move TypeScript rules to the TS-specific configuration block
    - Add comments
    - Move import plugin untyped config to the global block to run on
      all files
    
    At this point, the code base has 1146 problems (1092 errors, 54 warnings).
    
    Prior to the eslint 9 and flat config migration, there were 4 errors
    and 32 warnings.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -7,19 +7,22 @@ import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default defineConfig(
     js.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-    tseslint.configs.strictTypeChecked,
-    reactHooksPlugin.configs.flat.recommended,
+    importPlugin.flatConfigs.recommended,
     reactPlugin.configs.flat.recommended,
     reactPlugin.configs.flat["jsx-runtime"],
+    reactHooksPlugin.configs.flat.recommended,
     {
+        // Custom configuration to apply to TypeScript files
         files: ["**/*.{ts,tsx}"],
-        // Only try import validation on TypeScript ESM files
+
+        // TypeScript variants of plugins to make sure types are OK
         extends: [
-            importPlugin.flatConfigs.recommended,
-            importPlugin.flatConfigs.typescript
+            importPlugin.flatConfigs.typescript,
+            tseslint.configs.recommendedTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+            tseslint.configs.strictTypeChecked,
         ],
+
         // Configuration used by typescript-eslint.
         // projectService intelligently finds the nearest tsconfig.json for each
         // TypeScript file that it needs to compile and get types for.
@@ -28,8 +31,19 @@ export default defineConfig(
                 projectService: true,
             },
         },
+
+        // Custom rules
+        rules: {
+            "@typescript-eslint/no-unused-vars": ["warn", {
+                args: "none",
+                ignoreRestSiblings: true,
+                varsIgnorePattern: "^_",
+            }],
+        }
     },
     {
+        // Custom configuration applied to all files.
+
         // Configuration used by eslint-plugin-react.
         // Automatically detect the version of React.
         settings: {
@@ -37,23 +51,17 @@ export default defineConfig(
                 version: "detect",
             },
         },
-    },
-    {
+
+        // Custom rules
         rules: {
             "prefer-arrow-callback": "warn",
             "prefer-template": "error",
-            // "react/no-typos": "error",
+            "react/no-typos": "error",
             "object-shorthand": "error",
             "prefer-const": "error",
             "no-new": "error",
-            // "react/self-closing-comp": "error",
+            "react/self-closing-comp": "error",
             "guard-for-in": "error",
-
-            "@typescript-eslint/no-unused-vars": ["warn", {
-                args: "none",
-                ignoreRestSiblings: true,
-                varsIgnorePattern: "^_",
-            }],
         },
     }
 );
