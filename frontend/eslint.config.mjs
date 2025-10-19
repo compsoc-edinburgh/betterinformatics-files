4136369895905d6f27c66cb6dcb80c24de3333ab
Yuto Takano <y.takano@sms.ed.ac.uk>
2025-10-20 01:37:46 +0200

    Remove deprecated formatting-related eslint rules (Prettier covers them)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,22 +3,23 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import importPlugin from "eslint-plugin-import";
 import reactPlugin from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default defineConfig(
     js.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     tseslint.configs.stylisticTypeChecked,
     tseslint.configs.strictTypeChecked,
-    reactHooks.configs.flat.recommended,
+    reactHooksPlugin.configs.flat.recommended,
     reactPlugin.configs.flat.recommended,
     reactPlugin.configs.flat["jsx-runtime"],
     {
-        files: ['**/*.{ts,tsx}'],
-        extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
-        // other configs...
-    },
-    {
+        files: ["**/*.{ts,tsx}"],
+        // Only try import validation on TypeScript ESM files
+        extends: [
+            importPlugin.flatConfigs.recommended,
+            importPlugin.flatConfigs.typescript
+        ],
         // Configuration used by typescript-eslint.
         // projectService intelligently finds the nearest tsconfig.json for each
         // TypeScript file that it needs to compile and get types for.
@@ -27,7 +28,8 @@ export default defineConfig(
                 projectService: true,
             },
         },
-
+    },
+    {
         // Configuration used by eslint-plugin-react.
         // Automatically detect the version of React.
         settings: {
@@ -38,16 +40,7 @@ export default defineConfig(
     },
     {
         rules: {
-            "comma-dangle": ["error", {
-                arrays: "always-multiline",
-                objects: "always-multiline",
-                imports: "always-multiline",
-                exports: "always-multiline",
-                functions: "always-multiline",
-            }],
-
             "prefer-arrow-callback": "warn",
-            "template-curly-spacing": "error",
             "prefer-template": "error",
             // "react/no-typos": "error",
             "object-shorthand": "error",
@@ -55,7 +48,6 @@ export default defineConfig(
             "no-new": "error",
             // "react/self-closing-comp": "error",
             "guard-for-in": "error",
-            "@typescript-eslint/no-redeclare": 0,
 
             "@typescript-eslint/no-unused-vars": ["warn", {
                 args: "none",
