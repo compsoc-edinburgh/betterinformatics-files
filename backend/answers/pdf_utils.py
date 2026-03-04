7156765f3f16ba21a2ea846d4aab22b36a4c4237
Burak Emir Sezen <burakemirsezen@gmail.com>
2026-03-04 14:41:07 +0100

    Remove unused argument to get_page_text

--- backend/answers/pdf_utils.py
+++ backend/answers/pdf_utils.py
@@ -12,7 +12,7 @@ import pypdfium2 as pdfium
 logger = logging.getLogger(__name__)
 
 
-def get_page_text(path_to_pdf, page, tmpdirname):
+def get_page_text(path_to_pdf, page):
     with pdfium.PdfDocument(path_to_pdf) as pdf:
         if page > len(pdf):
             logger.warning(
@@ -41,23 +41,21 @@ def analyze_pdf(
     path_to_pdf,
     ExamPage=ExamPageModel,
 ):
-    base_path = settings.COMSOL_UPLOAD_FOLDER
     try:
-        with tempfile.TemporaryDirectory(dir=base_path) as tmpdirname:
-            ExamPage.objects.filter(exam=exam).delete()
-            with pdfium.PdfDocument(path_to_pdf) as pdf:
-                for page_number in range(1, len(pdf) + 1):
-                    page = pdf[page_number - 1]
-                    w, h = page.get_size()
-                    page_text = get_page_text(path_to_pdf, page_number, tmpdirname)
-                    exam_page = ExamPage(
-                        exam=exam,
-                        page_number=page_number,
-                        width=w,
-                        height=h,
-                        text=page_text,
-                    )
-                    exam_page.save()
+        ExamPage.objects.filter(exam=exam).delete()
+        with pdfium.PdfDocument(path_to_pdf) as pdf:
+            for page_number in range(1, len(pdf) + 1):
+                page = pdf[page_number - 1]
+                w, h = page.get_size()
+                page_text = get_page_text(path_to_pdf, page_number)
+                exam_page = ExamPage(
+                    exam=exam,
+                    page_number=page_number,
+                    width=w,
+                    height=h,
+                    text=page_text,
+                )
+                exam_page.save()
 
         return True
     except (FileNotFoundError, pdfium.PdfiumError) as e:
