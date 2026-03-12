85f3cd8e910ab2bd9b30b5381c206424f3fbbe50
Luca Schnellmann <lschnellman@student.ethz.ch>
2026-03-12 18:56:05 +0100

    Support .typ extension

--- frontend/src/components/create-document-file-modal.tsx
+++ frontend/src/components/create-document-file-modal.tsx
@@ -46,7 +46,7 @@ const CreateDocumentFileModal: React.FC<Props> = ({
           leftSection={<IconCloudUpload />}
           value={file}
           onChange={setFile}
-          accept=".pdf,.tex,.md,.txt,.zip,.apkg,.colpkg" // apkg=anki
+          accept=".pdf,.tex,.md,.typ,.txt,.zip,.apkg,.colpkg" // apkg=anki
         />
         <div>
           If you don't select any file we will create an empty markdown file for
