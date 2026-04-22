ed76cbd616e94480d12cab560d9b25dd5536d323
Yuto Takano <y.takano@sms.ed.ac.uk>
2026-04-22 15:29:12 +0200

    Use compsocMain for no-exams alert for consistency with no-documents alert

--- frontend/src/components/exam-list.tsx
+++ frontend/src/components/exam-list.tsx
@@ -137,7 +137,7 @@ const ExamList: React.FC<ExamListProps> = ({ metaData }) => {
         )
       }
       {viewableExams && viewableExams.length === 0 && (
-        <Alert variant="light" color="orange">
+        <Alert variant="light" color="compsocMain">
           No exams available to view.
         </Alert>
       )}
