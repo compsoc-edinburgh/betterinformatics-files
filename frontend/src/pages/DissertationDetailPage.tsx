b96feb681d0b1426ac89e716aa4b3c748fadb71f
Yuto Takano <y.takano@sms.ed.ac.uk>
2026-04-22 15:01:32 +0200

    Add placeholder /dissertation/:id/edit route to frontend

--- frontend/src/pages/DissertationDetailPage.tsx
+++ frontend/src/pages/DissertationDetailPage.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import {
   Container,
   Title,
@@ -101,123 +108,143 @@ const DissertationDetailPage: React.FC = () => {
         </Anchor>
       </Breadcrumbs>
 
-      <Flex justify="space-between">
-        <Stack gap={0}>
-          <Title order={1}>{dissertation.title}</Title>
-          <Text size="lg" mb="md" c="dimmed">
-            {dissertation.uploaded_by} - {dissertation.year}{" "}
-            {dissertation.study_level} Dissertation
-          </Text>
-        </Stack>
-
-        <Group>
-          <IconButton
-            color="gray"
-            icon={<IconDownload />}
-            tooltip="Download"
-            onClick={() => window.open(pdfUrl, "_blank")}
-          />
-          {(user?.username === dissertation.uploaded_by || user?.isAdmin) && (
+      <Routes>
+        <Route
+          path="edit"
+          element={!user?.isAdmin ? <Navigate to="." replace /> : <h1>hi</h1>}
+        />
+        <Route
+          path="/"
+          element={
             <>
-              <IconButton
-                color="gray"
-                icon={<IconEdit />}
-                tooltip="Edit"
-                onClick={() => navigate("edit")}
-              />
+              <Flex justify="space-between">
+                <Stack gap={0}>
+                  <Title order={1}>{dissertation.title}</Title>
+                  <Text size="lg" mb="md" c="dimmed">
+                    {dissertation.uploaded_by} - {dissertation.year}{" "}
+                    {dissertation.study_level} Dissertation
+                  </Text>
+                </Stack>
+
+                <Group>
+                  <IconButton
+                    color="gray"
+                    icon={<IconDownload />}
+                    tooltip="Download"
+                    onClick={() => window.open(pdfUrl, "_blank")}
+                  />
+                  {(user?.username === dissertation.uploaded_by ||
+                    user?.isAdmin) && (
+                    <>
+                      <IconButton
+                        color="gray"
+                        icon={<IconEdit />}
+                        tooltip="Edit"
+                        onClick={() => navigate("edit")}
+                      />
+                    </>
+                  )}
+                </Group>
+              </Flex>
+
+              <Table
+                layout="fixed"
+                variant="vertical"
+                verticalSpacing="4px"
+                withTableBorder
+                mb="xs"
+              >
+                <Table.Tbody>
+                  <Table.Tr>
+                    <Table.Th w={160}>Topic Tags</Table.Th>
+                    <Table.Td>
+                      <Group gap={4}>
+                        {dissertation.field_of_study
+                          .split(",")
+                          .map((field, index) => (
+                            <Badge
+                              key={index}
+                              variant="light"
+                              style={{ cursor: "pointer" }}
+                            >
+                              {field.trim()}
+                            </Badge>
+                          ))}
+                      </Group>
+                    </Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th style={{ verticalAlign: "top" }}>
+                      Relevant Courses
+                    </Table.Th>
+                    <Table.Td>
+                      <Stack gap={4} align="flex-start">
+                        {dissertation.relevant_categories.map(
+                          (category, index) => (
+                            <Anchor
+                              fz="sm"
+                              c="blue"
+                              key={index}
+                              component={Link}
+                              to={`/category/${category.slug}`}
+                            >
+                              {category.displayname}
+                            </Anchor>
+                          ),
+                        )}
+                      </Stack>
+                    </Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Primary Supervisors</Table.Th>
+                    <Table.Td>{dissertation.supervisors}</Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Year Written</Table.Th>
+                    <Table.Td>{dissertation.year}</Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Uploaded By</Table.Th>
+                    <Table.Td>{dissertation.uploaded_by}</Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Dissertation Level</Table.Th>
+                    <Table.Td>{dissertation.study_level}</Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Received Grade Band</Table.Th>
+                    {dissertation.grade_band && (
+                      <Table.Td>{dissertation.grade_band}</Table.Td>
+                    )}
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Additional Notes</Table.Th>
+                    {dissertation.notes && (
+                      <Table.Td>{dissertation.notes}</Table.Td>
+                    )}
+                  </Table.Tr>
+                </Table.Tbody>
+              </Table>
+
+              <Paper shadow="sm" p="sm" withBorder style={{ height: "80vh" }}>
+                {pdfUrl ? (
+                  <iframe
+                    src={pdfUrl}
+                    width="100%"
+                    height="100%"
+                    style={{ border: "none" }}
+                    title={dissertation.title}
+                  />
+                ) : (
+                  <Text ta="center" c="red">
+                    Failed to load PDF.
+                  </Text>
+                )}
+              </Paper>
             </>
-          )}
-        </Group>
-      </Flex>
-
-      <Table
-        layout="fixed"
-        variant="vertical"
-        verticalSpacing="4px"
-        withTableBorder
-        mb="xs"
-      >
-        <Table.Tbody>
-          <Table.Tr>
-            <Table.Th w={160}>Topic Tags</Table.Th>
-            <Table.Td>
-              <Group gap={4}>
-                {dissertation.field_of_study.split(",").map((field, index) => (
-                  <Badge
-                    key={index}
-                    variant="light"
-                    style={{ cursor: "pointer" }}
-                  >
-                    {field.trim()}
-                  </Badge>
-                ))}
-              </Group>
-            </Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th style={{ verticalAlign: "top" }}>
-              Relevant Courses
-            </Table.Th>
-            <Table.Td>
-              <Stack gap={4} align="flex-start">
-                {dissertation.relevant_categories.map((category, index) => (
-                  <Anchor
-                    fz="sm"
-                    c="blue"
-                    key={index}
-                    component={Link}
-                    to={`/category/${category.slug}`}
-                  >
-                    {category.displayname}
-                  </Anchor>
-                ))}
-              </Stack>
-            </Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th>Primary Supervisors</Table.Th>
-            <Table.Td>{dissertation.supervisors}</Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th>Year Written</Table.Th>
-            <Table.Td>{dissertation.year}</Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th>Uploaded By</Table.Th>
-            <Table.Td>{dissertation.uploaded_by}</Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th>Dissertation Level</Table.Th>
-            <Table.Td>{dissertation.study_level}</Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th>Received Grade Band</Table.Th>
-            {dissertation.grade_band && (
-              <Table.Td>{dissertation.grade_band}</Table.Td>
-            )}
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th>Additional Notes</Table.Th>
-            {dissertation.notes && <Table.Td>{dissertation.notes}</Table.Td>}
-          </Table.Tr>
-        </Table.Tbody>
-      </Table>
-
-      <Paper shadow="sm" p="sm" withBorder style={{ height: "80vh" }}>
-        {pdfUrl ? (
-          <iframe
-            src={pdfUrl}
-            width="100%"
-            height="100%"
-            style={{ border: "none" }}
-            title={dissertation.title}
-          />
-        ) : (
-          <Text ta="center" c="red">
-            Failed to load PDF.
-          </Text>
-        )}
-      </Paper>
+          }
+        />
+      </Routes>
     </Container>
   );
 };
