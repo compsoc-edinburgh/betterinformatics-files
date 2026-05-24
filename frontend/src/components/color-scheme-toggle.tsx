8d33515da92edb95ce244979c73b256832db4b53
Yuto Takano <y.takano@sms.ed.ac.uk>
2026-05-24 19:55:16 +0200

    Use children prop instead of leftSection prop for color scheme icons

--- frontend/src/components/color-scheme-toggle.tsx
+++ frontend/src/components/color-scheme-toggle.tsx
@@ -8,20 +8,51 @@ import {
 import { IconMoon, IconSun, IconSunMoon } from "@tabler/icons-react";
 
 const ColorSchemeToggle = () => {
-  const { colorScheme, setColorScheme, clearColorScheme } = useMantineColorScheme();
+  const { colorScheme, setColorScheme, clearColorScheme } =
+    useMantineColorScheme();
   return (
     <Group justify="center">
-      <HoverCard shadow="md" withArrow openDelay={100} closeDelay={100} disabled={colorScheme == "auto"}>
+      <HoverCard
+        shadow="md"
+        withArrow
+        openDelay={100}
+        closeDelay={100}
+        disabled={colorScheme == "auto"}
+      >
         <HoverCard.Target>
           <Group>
-            <Button darkHidden variant="transparent" leftSection={<IconMoon />} onClick={() => { setColorScheme("dark") }} />
-            <Button lightHidden variant="transparent" leftSection={<IconSun />} onClick={() => { setColorScheme("light") }} />
+            <Button
+              darkHidden
+              variant="transparent"
+              onClick={() => {
+                setColorScheme("dark");
+              }}
+              pr={0}
+            >
+              <IconMoon />
+            </Button>
+            <Button
+              lightHidden
+              variant="transparent"
+              onClick={() => {
+                setColorScheme("light");
+              }}
+              pr={0}
+            >
+              <IconSun />
+            </Button>
           </Group>
         </HoverCard.Target>
         <HoverCard.Dropdown>
           <Group>
             <Text>Follow system theme: </Text>
-            <Button variant="transparent" leftSection={<IconSunMoon />} onClick={() => { setColorScheme("auto") }} />
+            <Button
+              variant="transparent"
+              leftSection={<IconSunMoon />}
+              onClick={() => {
+                setColorScheme("auto");
+              }}
+            />
           </Group>
         </HoverCard.Dropdown>
       </HoverCard>
