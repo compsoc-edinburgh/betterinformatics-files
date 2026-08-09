e0f75c4efe0e52fb1013f40ee3a78d8b5203bb25
Yuto Takano <y.takano@sms.ed.ac.uk>
2026-08-10 00:52:46 +0200

    Show editor components when PageArticle editing is true

--- frontend/src/components/page-article.tsx
+++ frontend/src/components/page-article.tsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { PageResponse } from "../api/model";
-import { Group, Paper, Title } from "@mantine/core";
+import { Group, Paper, TextInput, Title } from "@mantine/core";
 import MarkdownText from "./markdown-text";
 import { IconMenu3 } from "@tabler/icons-react";
 import style from "./page-article.module.css";
+import Editor from "./Editor";
+import { UndoStack } from "./Editor/utils/undo-stack";
 
 export const PageArticle: React.FC<{
   page: PageResponse;
   editing: boolean;
   setEditing: (editing: boolean) => void;
-}> = ({ page, editing, setEditing }) => {
+}> = ({ page, editing }) => {
+  const [editingTitle, setEditingTitle] = useState(page.title);
+  const [editingContent, setEditingContent] = useState(page.content);
+  const [undoStack, setUndoStack] = useState<UndoStack>({ prev: [], next: [] });
+
   return (
     <Paper
       p={0}
-      pl="lg"
+      pl={editing ? "xs" : "lg"}
+      pr="md"
       radius={0}
       shadow="none"
       className={
         style.pageArticle /* Component must be Paper to use var(--paper-border-color) */
       }
     >
-      <Title order={1} mb="md">
-        {page.title}
-      </Title>
+      {editing ? (
+        <TextInput
+          placeholder="Title"
+          value={editingTitle}
+          onChange={e => setEditingTitle(e.target.value)}
+          classNames={{
+            input: style.editingTitle,
+          }}
+          size="lg"
+        />
+      ) : (
+        <Title order={1} mb="md">
+          {page.title}
+        </Title>
+      )}
       {page.category && (
         <>
           <Group gap="xs" mb="md">
@@ -38,7 +57,21 @@ export const PageArticle: React.FC<{
           </Group>
         </>
       )}
-      <MarkdownText value={page.content} />
+      {editing ? (
+        <Editor
+          value={editingContent}
+          allowOfficialAnswer={false}
+          onChange={setEditingContent}
+          imageHandler={file => {
+            throw new Error("Function not implemented");
+          }}
+          undoStack={undoStack}
+          setUndoStack={setUndoStack}
+          preview={value => <MarkdownText value={value} />}
+        />
+      ) : (
+        <MarkdownText value={page.content} />
+      )}
     </Paper>
   );
 };
