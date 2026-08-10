99524da732e01d0e87b56fadced2f5bb8b559e11
Yuto Takano <y.takano@sms.ed.ac.uk>
2026-08-10 22:18:43 +0200

    Stylise page article revision history as collapsible list

--- frontend/src/components/page-article-history.tsx
+++ frontend/src/components/page-article-history.tsx
@@ -1,8 +1,19 @@
-import { Alert, Paper } from "@mantine/core";
+import {
+  Alert,
+  Collapse,
+  Group,
+  Paper,
+  Text,
+  Stack,
+  ActionIcon,
+} from "@mantine/core";
 import { useUser } from "../auth";
 import { PageResponse } from "../api/model";
 import { useListRevisions } from "../api/hooks/pages";
+import { useState } from "react";
 import CodeBlock from "./code-block";
+import { IconChevronRight } from "@tabler/icons-react";
+import { formatISO } from "date-fns";
 
 export const PageArticleHistory: React.FC<{
   page: PageResponse;
@@ -10,6 +21,10 @@ export const PageArticleHistory: React.FC<{
   const user = useUser();
 
   const { data: revisions, isLoading, isError } = useListRevisions(page.slug);
+
+  const [expandedRevisions, setExpandedRevisions] = useState<Set<number>>(
+    new Set(),
+  );
 
   // Revisions only visible if logged in
   if (!user?.loggedin) {
@@ -33,21 +48,39 @@ export const PageArticleHistory: React.FC<{
   }
 
   return (
-    <Paper flex={1}>
+    <Paper flex={1} style={{ overflow: "auto" }}>
       {revisions?.revisions.map(revision => (
-        <Paper key={revision.id} p="md" mb="sm" shadow="xs">
-          {revision.message} - {revision.author.username} -{" "}
-          {new Date(revision.created_at).toLocaleString()}
-          <CodeBlock
-            value={revision.content_delta}
-            language="diff"
-            customStyle={{
-              lineHeight: 1,
-              overflowX: "hidden",
-              maxWidth: "100%",
+        <Stack key={revision.id} gap={0} w="100%">
+          <Group
+            onClick={() => {
+              const newExpandedRevisions = new Set(expandedRevisions);
+              if (!expandedRevisions.has(revision.id)) {
+                newExpandedRevisions.add(revision.id);
+              } else {
+                newExpandedRevisions.delete(revision.id);
+              }
+              setExpandedRevisions(newExpandedRevisions);
             }}
-          />
-        </Paper>
+          >
+            <ActionIcon variant="transparent">
+              <IconChevronRight
+                size={16}
+                style={{
+                  transform: expandedRevisions.has(revision.id)
+                    ? "rotate(90deg)"
+                    : "none",
+                }}
+              />
+            </ActionIcon>
+            <Text>
+              {formatISO(new Date(revision.created_at))} -{" "}
+              {revision.author.username} <i>({revision.message})</i>
+            </Text>
+          </Group>
+          <Collapse expanded={expandedRevisions.has(revision.id)}>
+            <CodeBlock value={revision.content_delta} language="diff" />
+          </Collapse>
+        </Stack>
       ))}
     </Paper>
   );
