b1419ea8d6221d00b22f3f6f52d2cb5a5edfb08b
Yuto Takano <y.takano@sms.ed.ac.uk>
2026-08-12 17:38:49 +0200

    Fix undefined evaluation order of scripts in static_html pages

--- frontend/src/components/extremely-trusted-html.tsx
+++ frontend/src/components/extremely-trusted-html.tsx
@@ -1,5 +1,67 @@
 import React, { useEffect, useRef } from "react";
 
+// https://html.spec.whatwg.org/multipage/scripting.html
+const runScriptTypes = [
+  "application/javascript",
+  "application/ecmascript",
+  "application/x-ecmascript",
+  "application/x-javascript",
+  "text/ecmascript",
+  "text/javascript",
+  "text/javascript1.0",
+  "text/javascript1.1",
+  "text/javascript1.2",
+  "text/javascript1.3",
+  "text/javascript1.4",
+  "text/javascript1.5",
+  "text/jscript",
+  "text/livescript",
+  "text/x-ecmascript",
+  "text/x-javascript",
+];
+
+const runScript = (script: HTMLScriptElement, cb: () => void) => {
+  const newScript = document.createElement("script");
+  newScript.type = script.type || "text/javascript";
+  if (script.src) {
+    newScript.onload = e => {
+      cb();
+      script.onload?.(e);
+    };
+    newScript.onerror = e => {
+      cb();
+      script.onerror?.(e);
+    };
+    newScript.src = script.src;
+  } else {
+    newScript.textContent = script.textContent;
+  }
+  document.head.appendChild(newScript);
+  script.parentNode?.removeChild(script);
+
+  if (!script.src) {
+    cb();
+  }
+};
+
+const sequence = (
+  fns: ((cb: () => void) => void)[],
+  cb: () => void,
+  index?: number,
+) => {
+  index ??= 0;
+
+  fns[index](() => {
+    index ??= 0;
+    index++;
+    if (index < fns.length) {
+      sequence(fns, cb, index);
+    } else {
+      cb();
+    }
+  });
+};
+
 /**
  * Render HTML and evaluate any scripts within it.
  * Only use for HTML when you are sure that if the submitter of HTML was
@@ -15,9 +77,28 @@ export const ExtremelyTrustedHTML: React.FC<{
 
   useEffect(() => {
     if (!ref.current) return;
-    const parsedHTML = document.createRange().createContextualFragment(html);
-    ref.current.innerHTML = "";
-    ref.current.appendChild(parsedHTML);
+    ref.current.innerHTML = html;
+    const scripts: ((cb: () => void) => void)[] = [];
+
+    ref.current.querySelectorAll("script").forEach(script => {
+      if (
+        script.getAttribute("type") !== null &&
+        !runScriptTypes.includes(script.type)
+      )
+        return;
+
+      scripts.push(cb => {
+        runScript(script, cb);
+      });
+    });
+
+    sequence(scripts, () => {
+      const event = new Event("DOMContentLoaded", {
+        bubbles: true,
+        cancelable: true,
+      });
+      document.dispatchEvent(event);
+    });
   }, [ref, html]);
 
   return <div ref={ref} />;
