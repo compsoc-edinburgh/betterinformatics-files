compsoc-edinburgh/betterinformatics-files

commit 7eae125178a7ba2f102c3990d80314eee1902cd8

Modify CourseStats.code to be FK to EuclidCode, add JSONB for percentiles

backend/categories/models.py
@@ -89,26 +89,28 @@ class EuclidCode(models.Model):
     # shadow courses (same course offered to UG/PG etc). This is used to both
     # show to users which courses on DRPS the category corresponds to, and
     # for admins, automatically analyse which courses are missing as categories.
-    code = models.CharField(max_length=12)
+    code = models.CharField(max_length=12, unique=True)
     category = models.ForeignKey(
-        "Category", related_name="euclid_codes", on_delete=models.CASCADE
+        "Category", related_name="euclid_codes", on_delete=models.CASCADE, null=True
     )
+
+    # Reverse type hints
+    stats: models.QuerySet["CourseStats"]  # disambiguate by academic_year
 
 
 class CourseStats(models.Model):
+    course_code: models.ForeignKey[EuclidCode] = models.ForeignKey(
+        "EuclidCode", related_name="stats", on_delete=models.CASCADE
+    )
     course_name = models.CharField(max_length=256)
-    course_code = models.CharField(max_length=12, db_index=True)
-    mean_mark = models.FloatField(null=True, blank=True)  # Can be null for N/A values
-    std_deviation = models.FloatField(
-        null=True, blank=True
-    )  # Can be null for N/A values
+    mean_mark = models.FloatField(null=True)
+    std_deviation = models.FloatField(null=True)
     academic_year = models.CharField(max_length=10)  # e.g. "2023-24"
-    course_organiser = models.CharField(
-        max_length=256, null=True, blank=True
-    )  # Course Organiser name
+    course_organiser = models.CharField(max_length=256, null=True)
+    percentiles = models.JSONField()
 
     class Meta:
         ordering = ["course_code", "academic_year"]
 
     def __str__(self):
-        return f"{self.course_code} ({self.academic_year}): {self.mean_mark}"
+        return f"{self.course_code.code} ({self.academic_year}): {self.mean_mark}"

backend/categories/views.py
@@ -520,14 +520,14 @@ def get_course_stats(request, slug):
         return response.success(value=[])
 
     # Get course stats for all Euclid codes associated with this category
-    stats = CourseStats.objects.filter(course_code__in=euclid_codes).order_by(
-        "course_code", "academic_year"
+    stats = CourseStats.objects.filter(course_code__code__in=euclid_codes).order_by(
+        "course_code__code", "academic_year"
     )
 
     res = [
         {
             "course_name": stat.course_name,
-            "course_code": stat.course_code,
+            "course_code": stat.course_code.code,
             "mean_mark": stat.mean_mark,
             "std_deviation": stat.std_deviation,
             "academic_year": stat.academic_year,

backend/categories/migrations/0021_add_percentile_stats_and_fk.py
@@ -1,0 +1,144 @@
+# Written manually on 2026.08.20. *Should* be revertible...
+
+# Changes the course_code field in CourseStats to an FK
+# Adds a JSONB field for percentile course stats
+# Some cleanup like adding uniqueness constraints and making field declarations
+#   consistent
+
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+def convert_to_link(apps, schema_editor):
+    CourseStats = apps.get_model("categories", "CourseStats")
+    EuclidCode = apps.get_model("categories", "EuclidCode")
+
+    for stats in CourseStats.objects.all():
+        code = stats.course_code
+        euclid_code = EuclidCode.objects.filter(code=code).first()
+        if euclid_code is None:
+            euclid_code = EuclidCode.objects.create(code=code, category=None)
+        stats.course_code_link = euclid_code
+        stats.save()
+
+def unconvert_from_link(apps, schema_editor):
+    CourseStats = apps.get_model("categories", "CourseStats")
+
+    for stats in CourseStats.objects.all():
+        stats.course_code = stats.course_code_link.code
+        stats.save()
+
+
+def delete_categoryless_codes(apps, schema_editor):
+    EuclidCode = apps.get_model("categories", "EuclidCode")
+    EuclidCode.objects.filter(category=None).delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('categories', '0020_examcounts_add_bitmask'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='coursestats',
+            name='percentiles',
+            field=models.JSONField(default={}),
+            preserve_default=False,
+        ),
+        # Enforce uniqueness
+        migrations.AlterField(
+            model_name='euclidcode',
+            name='code',
+            field=models.CharField(max_length=12, unique=True),
+        ),
+        # Add None to allow euclid code without a category
+        # (Needed since some course stats are not categories, like minf thesis)
+        migrations.AlterField(
+            model_name='euclidcode',
+            name='category',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='euclid_codes', to='categories.category'),
+        ),
+
+        # Reverse constraint flush in case we need to roll back the migration
+        migrations.RunSQL(
+            sql=migrations.RunSQL.noop,
+            reverse_sql="SET CONSTRAINTS ALL IMMEDIATE",
+        ),
+
+        # On the reverse path, delete any EuclidCodes without a category
+        migrations.RunPython(code=migrations.RunPython.noop, reverse_code=delete_categoryless_codes),
+
+        # Change course_code to foreignkey
+        # steps as described in https://stackoverflow.com/a/36000084
+        migrations.AddField(
+            model_name='coursestats',
+            name='course_code_link',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='stats',
+                to='categories.euclidcode',
+                # Temporarily allow null during this migration
+                null=True,
+            ),
+        ),
+        # Reverse constraint flush in case we need to roll back the migration
+        migrations.RunSQL(
+            sql=migrations.RunSQL.noop,
+            reverse_sql="SET CONSTRAINTS ALL IMMEDIATE",
+        ),
+        migrations.RunPython(code=convert_to_link, reverse_code=unconvert_from_link),
+        # Need to flush the constraints before we can fiddle around with the
+        # FK fields, otherwise Django complains about not wanting to alter table
+        # with pending trigger events.
+        migrations.RunSQL(
+            sql="SET CONSTRAINTS ALL IMMEDIATE",
+            reverse_sql=migrations.RunSQL.noop,
+        ),
+        # Set a default value for the course_code field, which doesn't do much
+        # in forward (since the field is removed in the next line) but is crucial
+        # for backwards migration, otherwise un-RemoveField will try to create
+        # a non-nullable field with no default value and fail
+        migrations.AlterField(
+            model_name='coursestats',
+            name='course_code',
+            field=models.CharField(max_length=12, db_index=True, default=''),
+        ),
+        migrations.RemoveField(
+            model_name='coursestats',
+            name='course_code',
+        ),
+        migrations.RenameField(
+            model_name='coursestats',
+            old_name='course_code_link',
+            new_name='course_code',
+        ),
+        migrations.AlterField(
+            model_name='coursestats',
+            name='course_code',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='stats',
+                to='categories.euclidcode',
+                # Disallow nulls
+                null=False,
+            ),
+        ),
+
+        # Remove blank=True (which only affects Django's admin interface)
+        migrations.AlterField(
+            model_name='coursestats',
+            name='course_organiser',
+            field=models.CharField(max_length=256, null=True),
+        ),
+        migrations.AlterField(
+            model_name='coursestats',
+            name='mean_mark',
+            field=models.FloatField(null=True),
+        ),
+        migrations.AlterField(
+            model_name='coursestats',
+            name='std_deviation',
+            field=models.FloatField(null=True),
+        ),
+    ]

commit 7bd48e59b25b9450cede883b1a9290fd3b6ec14f

Fix add/remove euclid code endpoints being broken after 7eae1251

backend/categories/models.py
@@ -90,7 +90,7 @@ class EuclidCode(models.Model):
     # show to users which courses on DRPS the category corresponds to, and
     # for admins, automatically analyse which courses are missing as categories.
     code = models.CharField(max_length=12, unique=True)
-    category = models.ForeignKey(
+    category: models.ForeignKey[Category | None] = models.ForeignKey(
         "Category", related_name="euclid_codes", on_delete=models.CASCADE, null=True
     )
 

backend/categories/views.py
@@ -474,11 +474,20 @@ def add_euclid_code(request, slug):
         return response.not_possible("Code too long")
 
     # Check if code is already assigned to another category, if so return the name of it
-    if cat.euclid_codes.filter(code=code).exists():
+    euclid_code = EuclidCode.objects.filter(code=code).first()
+    if euclid_code and euclid_code.category and euclid_code.category != cat:
         return response.not_possible(
-            f"Code already assigned to category {cat.euclid_codes.get(code=code).category.displayname}"
+            f"Code already assigned to category {euclid_code.category.displayname}"
         )
-    cat.euclid_codes.create(code=code)
+
+    if euclid_code and not euclid_code.category:
+        # Still not taken
+        euclid_code.category = cat
+        euclid_code.save()
+
+    if not euclid_code:
+        cat.euclid_codes.create(code=code, category=cat)
+
     return response.success()
 
 
@@ -487,7 +496,7 @@ def add_euclid_code(request, slug):
 def remove_euclid_code(request, slug):
     cat = get_object_or_404(Category, slug=slug)
     code = request.POST["code"].upper()
-    cat.euclid_codes.filter(code=code).delete()
+    cat.euclid_codes.filter(code=code).update(category=None)
     return response.success()
 
 
@@ -500,7 +509,11 @@ def get_category_from_euclid_code(request):
 
 @response.request_get()
 def list_euclid_codes(request):
-    codes = EuclidCode.objects.all()
+    codes = (
+        EuclidCode.objects.all()
+        .filter(category__isnull=False)
+        .select_related("category")
+    )
     res = [
         {
             "code": code.code,