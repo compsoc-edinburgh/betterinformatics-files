579512ac9641ecd8d500696519cc4d62fa79e9a9
Yuto Takano <y.takano@sms.ed.ac.uk>
2026-09-04 19:14:23 +0200

    Add courses.json endpoint for serving S3 content if it exists

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,10 +1,13 @@
 import logging
 import sys
 
+import yaml
+from botocore.exceptions import ClientError
+from django.http import JsonResponse
 from django.utils.cache import patch_cache_control
 from django.views.static import serve
 
-from util import response
+from util import response, s3_util
 
 logger = logging.getLogger(__name__)
 
@@ -30,4 +33,27 @@ def cached_serve(request, path, document_root=None, show_indexes=False):
     res = serve(request, path, document_root, show_indexes)
     DAY = 60 * 60 * 24
     patch_cache_control(res, public=True, max_age=30 * DAY)
+    return res
+
+
+def courses_json(request):
+    try:
+        # Get courses.yaml from S3
+        data = s3_util.s3_client.get_object(
+            Bucket=s3_util.s3_bucket_name,
+            Key="courses.yaml",
+        )
+    except ClientError:
+        return response.not_found()
+
+    try:
+        # Convert YAML to JSON
+        converted = yaml.safe_load(data["Body"].read())
+    except yaml.YAMLError as e:
+        logger.error("Error parsing courses.yaml: %s", e)
+        return response.internal_error()
+
+    res = JsonResponse(converted, safe=False)
+    patch_cache_control(res, public=True, max_age=60 * 60 * 24)
+    res["Access-Control-Allow-Origin"] = "*"
     return res
